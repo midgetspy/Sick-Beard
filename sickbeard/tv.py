776fe1d7f6a3eb703c9dadd671a76c0962ee1613
Nic Wolfe <nic@wolfeden.ca>
2009-11-07 07:51:44 +0000

    Not catching all the exceptions that I should be

--- sickbeard/tv.py
+++ sickbeard/tv.py
@@ -893,7 +893,7 @@ class TVEpisode:
 		
 		try:
 			myEp = t[self.show.tvdbid][season][episode]
-		except tvdb_exceptions.tvdb_episodenotfound:
+		except (tvdb_exceptions.tvdb_episodenotfound, tvdb_exceptions.tvdb_seasonnotfound):
 			Logger().log("Unable to find the episode on tvdb... has it been removed? Should I delete from db?")
 			return
 		
