357a78ac3a3ca60429a459bce58369f917da258d
Nic Wolfe <nic@wolfeden.ca>
2009-11-07 18:42:57 +0000

    Added GPL stuff

--- sickbeard/providers/tvnzb.py
+++ sickbeard/providers/tvnzb.py
@@ -1,14 +1,34 @@
+# Author: Nic Wolfe <nic@wolfeden.ca>
+# URL: http://code.google.com/p/sickbeard/
+#
+# This file is part of SickBeard.
+#
+# SickBeard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SickBeard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with SickBeard.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 import urllib
 import urllib2
 import os.path
 
 from lib.BeautifulSoup import BeautifulStoneSoup
 
-import midgetpvr
-import midgetpvr.classes
-import midgetpvr.helpers
+import sickbeard
+import sickbeard.classes
+import sickbeard.helpers
 
-from midgetpvr.common import *
+from sickbeard.common import *
 
 def downloadNZB (nzb):
 	pass
