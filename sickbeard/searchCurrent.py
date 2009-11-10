e46eb09e31dd1d97e0b8b155ac0f2de597ae3d45
Nic Wolfe <nic@wolfeden.ca>
2009-11-10 05:59:18 +0000

    Fixed some comments

--- sickbeard/searchCurrent.py
+++ sickbeard/searchCurrent.py
@@ -1,20 +1,20 @@
 # Author: Nic Wolfe <nic@wolfeden.ca>
 # URL: http://code.google.com/p/sickbeard/
 #
-# This file is part of SickBeard.
+# This file is part of Sick Beard.
 #
-# SickBeard is free software: you can redistribute it and/or modify
+# Sick Beard is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SickBeard is distributed in the hope that it will be useful,
+# Sick Beard is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 # 
 # You should have received a copy of the GNU General Public License
-# along with SickBeard.  If not, see <http://www.gnu.org/licenses/>.
+# along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
 
 
 
