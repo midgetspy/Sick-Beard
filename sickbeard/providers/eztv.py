d95743b1985aab55c0da81f1802831481370989c
Nic Wolfe <nic@wolfeden.ca>
2010-01-19 19:46:51 +0000

    Attempted to fix EZTV provider putting torrents in the wrong spot

--- sickbeard/providers/eztv.py
+++ sickbeard/providers/eztv.py
@@ -35,7 +35,7 @@ def downloadTorrent (torrent):
     if data == None:
         return False
     
-    fileName = torrent.fileName()
+    fileName = os.path.join(sickbeard.TORRENT_DIR, torrent.fileName())
     #fileName = os.path.join(sickbeard.TORRENT_DIR, os.path.basename(torrent.url))
     
     Logger().log("Saving to " + fileName, DEBUG)
