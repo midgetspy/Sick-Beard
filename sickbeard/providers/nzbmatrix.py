6e8118f5b347713822fcabe8e0a07e089e0356df
Nic Wolfe <nic@wolfeden.ca>
2010-01-26 07:54:59 +0000

    Changed nzbmatrix to https

--- sickbeard/providers/nzbmatrix.py
+++ sickbeard/providers/nzbmatrix.py
@@ -79,7 +79,7 @@ def findEpisode (episode, forceQuality=None):
 		params = {"search": curString.replace("."," "), "age": sickbeard.USENET_RETENTION, "username": sickbeard.NZBMATRIX_USERNAME, "apikey": sickbeard.NZBMATRIX_APIKEY}
 		params.update(quality)
 		
-		searchURL = "http://nzbmatrix.com/api-nzb-search.php?" + urllib.urlencode(params)
+		searchURL = "https://nzbmatrix.com/api-nzb-search.php?" + urllib.urlencode(params)
 	
 		Logger().log("Search string: " + searchURL, DEBUG)
 
