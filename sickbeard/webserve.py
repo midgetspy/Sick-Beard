d93a7bb3b07222376cc13910470f41eebe7deba8
Nic Wolfe <nic@wolfeden.ca>
2010-05-10 14:50:06 -0600

    Made "Snatched (Backlog)" episodes able to be set to "Downloaded" manually.

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -1337,8 +1337,8 @@ class Home:
                         logger.log("Refusing to change status of "+curEp+" because it is UNAIRED", logger.ERROR)
                         continue
                     
-                    if int(status) == DOWNLOADED and epObj.status not in (PREDOWNLOADED, SNATCHED_PROPER):
-                        logger.log("Refusing to change status of "+curEp+" to DOWNLOADED because it's not PREDOWNLOADED/SNATCHED_PROPER", logger.ERROR)
+                    if int(status) == DOWNLOADED and epObj.status not in (PREDOWNLOADED, SNATCHED_PROPER, SNATCHED_BACKLOG):
+                        logger.log("Refusing to change status of "+curEp+" to DOWNLOADED because it's not PREDOWNLOADED/SNATCHED_PROPER/SNATCHED_BACKLOG", logger.ERROR)
                         continue
 
                     epObj.status = int(status)
