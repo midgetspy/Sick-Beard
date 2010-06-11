32304d05e90c51dae448513e971070dc0f0fe851
Matthew Schick <matthew.schick@gmail.com>
2010-06-11 22:27:12 +0800

    Do encoding at the right spot
    Signed-off-by: Matthew Schick <matthew.schick@gmail.com>

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -1263,7 +1263,7 @@ class Home:
                 t.submenu.append({ 'title': 'Delete',            'path': 'home/deleteShow?show=%d'%showObj.tvdbid         })
                 t.submenu.append({ 'title': 'Re-scan files',           'path': 'home/refreshShow?show=%d'%showObj.tvdbid         })
                 t.submenu.append({ 'title': 'Force Full Update', 'path': 'home/updateShow?show=%d&force=1'%showObj.tvdbid })
-                t.submenu.append({ 'title': 'Update show in XBMC', 'path': 'home/updateXBMC?showName=%s'%showObj.name, 'requires': haveXBMC })
+                t.submenu.append({ 'title': 'Update show in XBMC', 'path': 'home/updateXBMC?showName=%s'%urllib.quote_plus(showObj.name), 'requires': haveXBMC })
             t.submenu.append({ 'title': 'Rename Episodes',   'path': 'home/fixEpisodeNames?show=%d'%showObj.tvdbid        })
         
         t.show = showObj
@@ -1454,13 +1454,8 @@ class Home:
     @cherrypy.expose
     def updateXBMC(self, showName=None):
 
-        if showName:
-            encShowName = urllib.quote_plus(showName)
-        else:
-            encShowName = None
-
         for curHost in [x.strip() for x in sickbeard.XBMC_HOST.split(",")]:
-            if xbmc.updateLibrary(curHost, showName=encShowName):
+            if xbmc.updateLibrary(curHost, showName=showName):
                 flash.message("Command sent to XBMC host " + curHost + " to update library")
             else:
                 flash.error("Unable to contact XBMC host " + curHost)
