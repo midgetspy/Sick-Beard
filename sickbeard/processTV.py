fe0abd36d766c9095595b6bf55859a507d43da13
Nic Wolfe <nic@wolfeden.ca>
2010-09-02 12:47:36 -0600

    Added fix from f1sherman's fork that got missed somehow, fixes post-processing bug

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -318,8 +318,10 @@ def processFile(fileName, downloadDir=None, nzbName=None):
             returnStr += logHelper("Looking up show in DB instead", logger.DEBUG)
             showInfo = helpers.searchDBForShow(result.seriesname)
 
-        if showInfo and season == None:
+        if showInfo:
             tvdb_id = showInfo[0]
+
+        if showInfo and season == None:
             myDB = db.DBConnection()
             numseasonsSQlResult = myDB.select("SELECT COUNT(DISTINCT season) as numseasons FROM tv_episodes WHERE showid = ? and season != 0", [tvdb_id])
             numseasons = numseasonsSQlResult[0][0]
