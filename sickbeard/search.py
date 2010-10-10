e62e19095fce0996d47b17997da506ec68f6ecda
Adam Bielicki <adam.bielicki@gmail.com>
2010-10-10 13:28:55 +0800

    caused exclusions to affect RSS feed updates

--- sickbeard/search.py
+++ sickbeard/search.py
@@ -131,6 +131,8 @@ def searchForNeededEpisodes():
 			logger.log(traceback.format_exc(), logger.DEBUG)
 			continue
 
+		curFoundResults = filter(lambda x: sceneHelpers.filterBadReleases(x.name) and isGoodResult(x, episode.show), curFoundResults)
+
 		didSearch = True
 		
 		# pick a single result for each episode, respecting existing results
