d21cf6ac08fe1942bbfd807fadd7f75e737a1489
Nic Wolfe <nic@wolfeden.ca>
2010-10-28 00:03:29 -0600

    Allow characters other than spaces when detecting the quality of existing episodes

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -115,7 +115,7 @@ class Quality:
             if x == Quality.UNKNOWN:
                 continue
 
-            regex = Quality.qualityStrings[x]
+            regex = Quality.qualityStrings[x].replace(' ','\W')
             regex_match = re.match(regex, name, re.I)
             if regex_match:
                 return x
