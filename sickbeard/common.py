1a6117cb33b30627d63900d43567482bc29789ac
Nic Wolfe <nic@wolfeden.ca>
2010-11-10 16:19:01 -0700

    Generalized web-dl matching for poorly named releases on NZBMatrix

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -128,7 +128,7 @@ class Quality:
             return Quality.SDDVD
         elif checkName(["720p", "hdtv", "x264"], all) or checkName(["hr.ws.pdtv.x264"], any):
             return Quality.HDTV
-        elif checkName(["720p.web.dl"], any) or checkName(["720p", "itunes", "h.?264"], all):
+        elif checkName(["720p", "web.dl"], all) or checkName(["720p", "itunes", "h.?264"], all):
             return Quality.HDWEBDL
         elif checkName(["720p", "bluray", "x264"], all):
             return Quality.HDBLURAY
