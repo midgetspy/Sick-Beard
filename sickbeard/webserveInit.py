28702b4b279a706d00e6c4ba2f93768c6afb872e
Nic Wolfe <nic@wolfeden.ca>
2010-11-18 23:24:54 -0700

    Don't log 401's as errors, just warnings

--- sickbeard/webserveInit.py
+++ sickbeard/webserveInit.py
@@ -25,8 +25,11 @@ def initWebServer(options = {}):
         #HTTP Errors
         def http_error_401_hander(status, message, traceback, version):
             args = [status, message, traceback, version]
-            logger.log(u"CherryPy caught an error: %s %s" % (status, message), logger.ERROR)
-            logger.log(traceback, logger.DEBUG)
+            if int(status) == 401:
+                logger.log(u"Authentication error, check cherrypy log for more details", logger.WARNING)
+            else:
+                logger.log(u"CherryPy caught an error: %s %s" % (status, message), logger.ERROR)
+                logger.log(traceback, logger.DEBUG)
             return "<html><body><h1>Error %s</h1>Something unexpected has happened. Please check the log.</body></html>" % args[0]
         cherrypy.config.update({'error_page.401' : http_error_401_hander})
 
