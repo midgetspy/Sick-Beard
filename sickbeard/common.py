2fd03f76b1aaa113b8326b11791f8fd877314f5b
Nic Wolfe <nic@wolfeden.ca>
2011-01-04 19:02:05 -0700

    Added an exception for Come Fly With Me

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -279,6 +279,7 @@ sceneExceptions = {72546: ['CSI'],
                    164451: ['Carlos (2010)'],
                    70726: ['Babylon 5', 'Babylon5'],
                    83714: ['Genius', 'Genius With Dave Gormand'],
+                   212571: ['Come Fly With Me (2010)'],
                    }
 
 countryList = {'Australia': 'AU',
