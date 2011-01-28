d9d85defd39e54c52d263e8caf77de0a0f37246e
Nic Wolfe <nic@wolfeden.ca>
2011-01-28 09:17:53 -0700

    Fix an import conflict with simplejson

--- lib/simplejson/scanner.py
+++ lib/simplejson/scanner.py
@@ -2,7 +2,7 @@
 """
 import re
 try:
-    from simplejson._speedups import make_scanner as c_make_scanner
+    from lib.simplejson._speedups import make_scanner as c_make_scanner
 except ImportError:
     c_make_scanner = None
 
