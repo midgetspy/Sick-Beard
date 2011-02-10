b96d5c9676c439d12b6edeb6c1b467e2d2148996
Jeroen Bouwens <jeroen@gargleblaster.lan>
2011-02-10 08:42:21 +0100

    Unfinished implementation of notifo notifications

---
+++ sickbeard/notifiers/notifo.py
@@ -0,0 +1,17 @@
+
+
+class NotifoNotifier:
+
+	def test_notify(self, host, apikey):
+		return true
+
+	def notify_snatch(self, ep_name):
+		return true
+
+	def notify_downloade(self, ep_name):
+		return true
+
+	def _notifyNotifo(self, message):
+		return true
+
+notifier = NotifoNotifier
