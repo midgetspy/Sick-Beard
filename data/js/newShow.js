b93a23461645b4613de67d9af4ab2353405c9a37
Nic Wolfe <nic@wolfeden.ca>
2011-02-12 15:56:14 -0700

    Make a global sbRoot javascript var so we can use it inside included js rather than putting them inline in the templates

--- data/js/newShow.js
+++ data/js/newShow.js
@@ -1,5 +1,68 @@
 $(document).ready(function(){
 
+    $.getJSON(sbRoot+'/home/addShows/getTVDBLanguages', {}, function(data){
+        var resultStr = '';
+
+        if (data.results.length == 0) {
+            resultStr = '<option value="en" selected="selected">en</option>';
+        } else {
+            $.each(data.results, function(index, obj){
+                if (resultStr == '')
+                        selected = ' selected="selected"';
+                else
+                        selected = '';
+
+                resultStr += '<option value="' + obj + '"' + selected + '>' + obj + '</option>';
+            });
+        }
+        $('#tvdbLangSelect').html(resultStr)
+
+        $('#tvdbLangSelect').change(function() { searchTvdb() });
+    });
+
+
+    function searchTvdb(){
+        $('#searchResults').html('<img id="searchingAnim" src="'+sbRoot+'/images/searching_anim.gif">searching..</img>');
+
+        $.getJSON(sbRoot+'/home/addShows/searchTVDBForShowName', {'name': $('#nameToSearch').val(), 'lang': $('#tvdbLangSelect').val()}, function(data){
+            var resultStr = '';
+            
+            if (data.results.length == 0) {
+                resultStr = '<b>No results found, try a different search.</b>';
+            } else {
+            
+                $.each(data.results, function(index, obj){
+                    if (resultStr == '')
+                        checked = ' checked';
+                    else
+                        checked = '';
+                    resultStr += '<input type="radio" name="whichSeries" value="' + obj[0] + '|' + obj[1] + '"' + checked + ' /> ';
+                    if(data.langid && data.langid != "")
+                            resultStr += '<a href="http://thetvdb.com/?tab=series&id=' + obj[0] + '&lid=' + data.langid + '" target="_new"><b>' + obj[1] + '</b></a>';
+                    else
+                        resultStr += '<a href="http://thetvdb.com/?tab=series&id=' + obj[0] + '" target="_new"><b>' + obj[1] + '</b></a>';
+
+                    if (obj[2] != null)
+                        resultStr += ' (started on ' + obj[2] + ')';
+                    resultStr += '<br />';
+                });
+                resultStr += '</ul>';
+            }
+            $('#searchResults').html(resultStr);
+        });
+    };  
+
+    $('#searchName').click(function() {searchTvdb()});
+
+    if ($('#nameToSearch').val().length)
+        $('#searchName').click()
+
+    $('#cancelShow').click(function(){
+        $('#cancelShowItem').val(1);
+        $('#addShowForm').submit();
+    });
+    
+
     $('#addShowButton').click(function(){
         
         // if they haven't picked a show don't let them submit
