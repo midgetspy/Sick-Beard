253150b4f94c7e812ae7f721724b6b92ebd2c961
Jonathon Saine <thezoggy@gmail.com>
2011-02-27 00:14:18 -0600

    Moved javascript from inc_top to it's own file so I could include it only on the pages I wanted.

---
+++ data/js/tableClick.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+   $("table.sickbeardTable td").hover( 
+       function() { $(this).find("a").parent().addClass("hover"); }, 
+       function() { $(this).find("a").parent().removeClass("hover");
+   } );
+
+   $("table.sickbeardTable td").click( function() {
+        var href = $(this).find("a").attr("href");
+        if(href) { window.location = href; }
+   });
+
+});
