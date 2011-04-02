6f72cb91d477ffa044cc81d844adb55e86efa93e
Josh Schreuder <joshschreuder@gmail.com>
2011-04-02 20:19:36 +1100

    Fix libnotify test button

--- data/js/configNotifications.js
+++ data/js/configNotifications.js
@@ -47,7 +47,7 @@ $(document).ready(function(){
 
     $('#testLibnotify').click(function(){
         $('#testLibnotify-result').html(loading);
-        $.get("$sbRoot/home/testLibnotify",
+        $.get(sbRoot+"/home/testLibnotify",
         function(message){ $('#testLibnotify-result').html(message); });
     });
   
