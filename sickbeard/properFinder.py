41508101fd98033aeb2633a44ceba953dad77732
Dennis Lutter <lad1337@gmail.com>
2011-04-14 23:07:22 +0200

    fix: hope this fixes the "list index out of range" for a proper search. deactivated proper seach processing for an anime result

--- sickbeard/properFinder.py
+++ sickbeard/properFinder.py
@@ -109,7 +109,12 @@ class ProperFinder():
                 curProper.episode = parse_result.air_date
             else:
                 curProper.season = parse_result.season_number if parse_result.season_number != None else 1
-                curProper.episode = parse_result.episode_numbers[0]
+                if parse_result.is_anime:
+                    logger.log(u"I am sorry '"+curProper.name+"' seams to be an anime proper seach is not yet suported", logger.DEBUG)
+                    continue
+                    curProper.episode = parse_result.ab_episode_numbers[0]
+                else:
+                    curProper.episode = parse_result.episode_numbers[0]
             
             curProper.quality = Quality.nameQuality(curProper.name,parse_result.is_anime)
 
