e58c581ba9881b6bfd6f916ee540b897c2ad3203
Giant Spook <giantspook@gmail.com>
2011-04-14 03:34:28 -0400

    Change db.py tabs to spaces for easier merging

--- sickbeard/db.py
+++ sickbeard/db.py
@@ -32,200 +32,198 @@ db_lock = threading.Lock()
 
 
 class DBResult(list):
-	'''
-	Sub-class of list which stores some SQL-query metadata along with the query
-	results.  The column_names attribute contains string versions of the column
-	names of the result table which can be used to perform dictionary lookup on
-	the sqlite3.Row objects the DBResult contains.
-	'''
-	def __init__(self, L, description):
-		'''
-		>>> r = DBResult([], (('a',), ('b',), ('c',)))
-		>>> r.column_names
-		('a', 'b', 'c')
-		'''
-		super(DBResult, self).__init__(L)
-		self.column_names = tuple(col[0] for col in description)
+    '''
+    Sub-class of list which stores some SQL-query metadata along with the query
+    results.  The column_names attribute contains string versions of the column
+    names of the result table which can be used to perform dictionary lookup on
+    the sqlite3.Row objects the DBResult contains.
+    '''
+    def __init__(self, L, description):
+        '''
+        >>> r = DBResult([], (('a',), ('b',), ('c',)))
+        >>> r.column_names
+        ('a', 'b', 'c')
+        '''
+        super(DBResult, self).__init__(L)
+        self.column_names = tuple(col[0] for col in description)
 
-	def __iadd__(self, other):
-		'''
-		>>> a = DBResult('12345', (('a',), ('b',), ('c',)))
-		>>> b = DBResult('67890', (('a',), ('b',), ('c',)))
-		>>> ''.join(a + b)
-		'1234567890'
-		'''
-		if self.column_names != other.column_names:
-			raise ValueError("Incompatible columns")
-		return super(DBResult, self).__iadd__(other)
+    def __iadd__(self, other):
+        '''
+        >>> a = DBResult('12345', (('a',), ('b',), ('c',)))
+        >>> b = DBResult('67890', (('a',), ('b',), ('c',)))
+        >>> ''.join(a + b)
+        '1234567890'
+        '''
+        if self.column_names != other.column_names:
+            raise ValueError("Incompatible columns")
+        return super(DBResult, self).__iadd__(other)
 
 
 class DBConnection:
-	'''
-	Convenience wrapper around an sqlite3 database that provides locking
-	for concurrent access.
+    '''
+    Convenience wrapper around an sqlite3 database that provides locking
+    for concurrent access.
 
-	>>> db = DBConnection(dbFileName=":memory:")
-	>>> db.action(None) is None
-	True
-	>>> r = db.action('CREATE TABLE foo ( col1 varchar(10), col2 int )')
-	>>> r = db.action('INSERT INTO foo VALUES ("bar", 1)')
-	>>> r = db.action('INSERT INTO foo VALUES ("biz", 2)')
-	>>> r = db.select('SELECT * FROM foo')
-	>>> r[0]['col1']
-	u'bar'
-	>>> r[1]['col2']
-	2
-	>>> r.column_names
-	('col1', 'col2')
-	'''
-	def __init__(self, dbFileName="sickbeard.db"):
+    >>> db = DBConnection(dbFileName=":memory:")
+    >>> db.action(None) is None
+    True
+    >>> r = db.action('CREATE TABLE foo ( col1 varchar(10), col2 int )')
+    >>> r = db.action('INSERT INTO foo VALUES ("bar", 1)')
+    >>> r = db.action('INSERT INTO foo VALUES ("biz", 2)')
+    >>> r = db.select('SELECT * FROM foo')
+    >>> r[0]['col1']
+    u'bar'
+    >>> r[1]['col2']
+    2
+    >>> r.column_names
+    ('col1', 'col2')
+    '''
+    def __init__(self, dbFileName="sickbeard.db"):
 
-		self.dbFileName = dbFileName
+        self.dbFileName = dbFileName
 
-		# Use an in-memory database for testing
-		if dbFileName == ":memory:":
-			dbPath = ":memory:"
-		else:
-			dbPath = os.path.join(sickbeard.DATA_DIR, self.dbFileName)
+        # Use an in-memory database for testing
+        if dbFileName == ":memory:":
+            dbPath = ":memory:"
+        else:
+            dbPath = os.path.join(sickbeard.DATA_DIR, self.dbFileName)
 
-		self.connection = sqlite3.connect(dbPath, 20)
-		self.connection.row_factory = sqlite3.Row
-		self.cursor = self.connection.cursor()
+        self.connection = sqlite3.connect(dbPath, 20)
+        self.connection.row_factory = sqlite3.Row
+        self.cursor = self.connection.cursor()
 
- 	def action(self, query, args=None):
+    def action(self, query, args=None):
 
- 		with db_lock:
+        with db_lock:
 
-			if query is None:
-				return None
-	
-			attempt = 0
-	
-			while attempt < 5:
-				try:
-					if args == None:
-						logger.log(self.dbFileName+": "+query, logger.DEBUG)
-						sqlResult = self.cursor.execute(query)
-					else:
-						logger.log(self.dbFileName+": "+query+" with args "+str(args), logger.DEBUG)
-						sqlResult = self.cursor.execute(query, args)
-					self.connection.commit()
-					# get out of the connection attempt loop since we were successful
-					return sqlResult
-				except sqlite3.OperationalError, e:
-					if "unable to open database file" in str(e) or "database is locked" in str(e):
-						logger.log(u"DB error: "+str(e).decode('utf-8'), logger.WARNING)
-						attempt += 1
-						time.sleep(1)
-					else:
-						logger.log(u"DB error: "+str(e).decode('utf-8'), logger.ERROR)
-						raise
-				except sqlite3.DatabaseError, e:
-					logger.log(u"Fatal error executing query: " + str(e), logger.ERROR)
-					raise
-	
-			return None
+            if query is None:
+                return None
+    
+            attempt = 0
+    
+            while attempt < 5:
+                try:
+                    if args == None:
+                        logger.log(self.dbFileName+": "+query, logger.DEBUG)
+                        sqlResult = self.cursor.execute(query)
+                    else:
+                        logger.log(self.dbFileName+": "+query+" with args "+str(args), logger.DEBUG)
+                        sqlResult = self.cursor.execute(query, args)
+                    self.connection.commit()
+                    # get out of the connection attempt loop since we were successful
+                    return sqlResult
+                except sqlite3.OperationalError, e:
+                    if "unable to open database file" in str(e) or "database is locked" in str(e):
+                        logger.log(u"DB error: "+str(e).decode('utf-8'), logger.WARNING)
+                        attempt += 1
+                        time.sleep(1)
+                    else:
+                        logger.log(u"DB error: "+str(e).decode('utf-8'), logger.ERROR)
+                        raise
+                except sqlite3.DatabaseError, e:
+                    logger.log(u"Fatal error executing query: " + str(e), logger.ERROR)
+                    raise
+    
+            return None
 
 
-	def select(self, query, args=None):
+    def select(self, query, args=None):
 
-		sqlResults = self.action(query, args).fetchall()
+        sqlResults = self.action(query, args).fetchall()
 
-		if sqlResults is None:
-			sqlResults = []
+        if sqlResults is None:
+            sqlResults = []
 
-		return DBResult(sqlResults, self.cursor.description)
+        return DBResult(sqlResults, self.cursor.description)
 
-	def upsert(self, tableName, valueDict, keyDict):
+    def upsert(self, tableName, valueDict, keyDict):
 
-		changesBefore = self.connection.total_changes
+        changesBefore = self.connection.total_changes
 
-		genParams = lambda myDict : [x + " = ?" for x in myDict.keys()]
+        genParams = lambda myDict : [x + " = ?" for x in myDict.keys()]
 
-		query = "UPDATE "+tableName+" SET " + ", ".join(genParams(valueDict)) + " WHERE " + " AND ".join(genParams(keyDict))
+        query = "UPDATE "+tableName+" SET " + ", ".join(genParams(valueDict)) + " WHERE " + " AND ".join(genParams(keyDict))
 
-		self.action(query, valueDict.values() + keyDict.values())
+        self.action(query, valueDict.values() + keyDict.values())
 
-		if self.connection.total_changes == changesBefore:
-			query = "INSERT INTO "+tableName+" (" + ", ".join(valueDict.keys() + keyDict.keys()) + ")" + \
-			         " VALUES (" + ", ".join(["?"] * len(valueDict.keys() + keyDict.keys())) + ")"
-			self.action(query, valueDict.values() + keyDict.values())
+        if self.connection.total_changes == changesBefore:
+            query = "INSERT INTO "+tableName+" (" + ", ".join(valueDict.keys() + keyDict.keys()) + ")" + \
+                     " VALUES (" + ", ".join(["?"] * len(valueDict.keys() + keyDict.keys())) + ")"
+            self.action(query, valueDict.values() + keyDict.values())
 
-	def tableInfo(self, tableName):
-		# FIXME ? binding is not supported here, but I cannot find a way to escape a string manually
-		cursor = self.connection.execute("PRAGMA table_info(%s)" % tableName)
-		columns = {}
-		for column in cursor:
-			columns[column['name']] = { 'type': column['type'] }
-		return columns
+    def tableInfo(self, tableName):
+        # FIXME ? binding is not supported here, but I cannot find a way to escape a string manually
+        cursor = self.connection.execute("PRAGMA table_info(%s)" % tableName)
+        columns = {}
+        for column in cursor:
+            columns[column['name']] = { 'type': column['type'] }
+        return columns
 
 def sanityCheckDatabase(connection, sanity_check):
-	sanity_check(connection).check()
+    sanity_check(connection).check()
 
 class DBSanityCheck(object):
-	def __init__(self, connection):
-		self.connection = connection
+    def __init__(self, connection):
+        self.connection = connection
 
-	def check(self):
-		pass
+    def check(self):
+        pass
 
 # ===============
 # = Upgrade API =
 # ===============
 
 def upgradeDatabase(connection, schema):
-	logger.log(u"Checking database structure...", logger.MESSAGE)
-	_processUpgrade(connection, schema)
+    logger.log(u"Checking database structure...", logger.MESSAGE)
+    _processUpgrade(connection, schema)
 
 def prettyName(str):
-	return ' '.join([x.group() for x in re.finditer("([A-Z])([a-z0-9]+)", str)])
+    return ' '.join([x.group() for x in re.finditer("([A-Z])([a-z0-9]+)", str)])
 
 def _processUpgrade(connection, upgradeClass):
-	instance = upgradeClass(connection)
-	logger.log(u"Checking " + prettyName(upgradeClass.__name__) + " database upgrade", logger.DEBUG)
-	if not instance.test():
-		logger.log(u"Database upgrade required: " + prettyName(upgradeClass.__name__), logger.MESSAGE)
-		try:
-			instance.execute()
-		except sqlite3.DatabaseError, e:
-			print "Error in " + str(upgradeClass.__name__) + ": " + str(e)
-			raise
-		logger.log(upgradeClass.__name__ + " upgrade completed", logger.DEBUG)
-	else:
-		logger.log(upgradeClass.__name__ + " upgrade not required", logger.DEBUG)
+    instance = upgradeClass(connection)
+    logger.log(u"Checking " + prettyName(upgradeClass.__name__) + " database upgrade", logger.DEBUG)
+    if not instance.test():
+        logger.log(u"Database upgrade required: " + prettyName(upgradeClass.__name__), logger.MESSAGE)
+        try:
+            instance.execute()
+        except sqlite3.DatabaseError, e:
+            print "Error in " + str(upgradeClass.__name__) + ": " + str(e)
+            raise
+        logger.log(upgradeClass.__name__ + " upgrade completed", logger.DEBUG)
+    else:
+        logger.log(upgradeClass.__name__ + " upgrade not required", logger.DEBUG)
 
-	for upgradeSubClass in upgradeClass.__subclasses__():
-		_processUpgrade(connection, upgradeSubClass)
+    for upgradeSubClass in upgradeClass.__subclasses__():
+        _processUpgrade(connection, upgradeSubClass)
 
 # Base migration class. All future DB changes should be subclassed from this class
 class SchemaUpgrade (object):
-	def __init__(self, connection):
-		self.connection = connection
+    def __init__(self, connection):
+        self.connection = connection
 
-	def hasTable(self, tableName):
-		return len(self.connection.action("SELECT 1 FROM sqlite_master WHERE name = ?;", (tableName, )).fetchall()) > 0
+    def hasTable(self, tableName):
+        return len(self.connection.action("SELECT 1 FROM sqlite_master WHERE name = ?;", (tableName, )).fetchall()) > 0
 
-	def hasColumn(self, tableName, column):
-		return column in self.connection.tableInfo(tableName)
+    def hasColumn(self, tableName, column):
+        return column in self.connection.tableInfo(tableName)
 
-	def addColumn(self, table, column, type="NUMERIC", default=0):
-		self.connection.action("ALTER TABLE %s ADD %s %s" % (table, column, type))
-		self.connection.action("UPDATE %s SET %s = ?" % (table, column), (default,))
+    def addColumn(self, table, column, type="NUMERIC", default=0):
+        self.connection.action("ALTER TABLE %s ADD %s %s" % (table, column, type))
+        self.connection.action("UPDATE %s SET %s = ?" % (table, column), (default,))
 
-	def checkDBVersion(self):
-		result = self.connection.select("SELECT db_version FROM db_version")
-		if result:
-			return int(result[0]["db_version"])
-		else:
-			return 0
+    def checkDBVersion(self):
+        result = self.connection.select("SELECT db_version FROM db_version")
+        if result:
+            return int(result[0]["db_version"])
+        else:
+            return 0
 
-	def incDBVersion(self):
-		curVersion = self.checkDBVersion()
-		self.connection.action("UPDATE db_version SET db_version = ?", [curVersion+1])
-		return curVersion+1
+    def incDBVersion(self):
+        curVersion = self.checkDBVersion()
+        self.connection.action("UPDATE db_version SET db_version = ?", [curVersion+1])
+        return curVersion+1
 
 if __name__ == '__main__':
-	import doctest
-	doctest.testmod()
-
-# vim: ft=python noexpandtab
+    import doctest
+    doctest.testmod()
