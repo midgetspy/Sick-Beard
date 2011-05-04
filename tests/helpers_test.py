d122a709edd335d393c8d1b2fd2b83fba651e600
Dennis Lutter <lad1337@gmail.com>
2011-05-04 09:34:59 +0200

    changes in test

--- tests/helpers_test.py
+++ tests/helpers_test.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('../lib'))
 
 import sickbeard # we need to import this so we can override the SYS_ENCODING which is needed by the parser
-from sickbeard import helpers
+from sickbeard import helpers, scene_exceptions
 
 DEBUG = VERBOSE = False
 sickbeard.SYS_ENCODING = "UTF-8"
@@ -28,14 +28,26 @@ class DumyTVShow(object):
 
 
 showDict = {DumyTVShow(1,"Dance in the Vampire Bund",True):"[SFW-Chihiro] Dance in the Vampire Bund - 12 [1920x1080 Blu-ray FLAC][2F6DBC66].mkv",
-                DumyTVShow(2,"Infinite Stratos",True):"[Stratos-Subs]_Infinite_Stratos_-_12_(1280x720_H.264_AAC)_[379759DB]",
-                DumyTVShow(3,"Dexter",True):"Dexter S05E07 1080p HDTV DD5.1 H.264",
-                }
+            DumyTVShow(2,"Infinite Stratos",True):"[Stratos-Subs]_Infinite_Stratos_-_12_(1280x720_H.264_AAC)_[379759DB]",
+            DumyTVShow(3,"Blue Exorcist",True):"[Stratos-Subs]_Ao no Exorcist_-_12_(1280x720)_[379759DB]",
+            DumyTVShow(4,"Dexter",True):"Dexter S05E07 1080p HDTV DD5.1 H.264",
+            }
 showList =[x for x,y in showDict.items()]
+
+showExceptions = {2:["IS"]
+                  }
+
+def dummy_get_scene_exceptions(id):
+    try:
+        return showExceptions[id]
+    except:
+        return []
+    
+scene_exceptions.get_scene_exceptions = dummy_get_scene_exceptions
 
 def generator_get_tvdbid(show,name,list):
     def test(self):
-        result = helpers.get_tvdbid(show.name, showList, False)
+        result = helpers.get_tvdbid(show.name,list, False)
         self.assertEqual(result, show.tvdbid)
     return test
 
