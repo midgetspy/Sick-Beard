fcb41b2ed0b06c4b0c5fa5821e4b3f4a20cbf193
Dennis Lutter <lad1337@gmail.com>
2011-05-06 23:39:16 +0200

    added a testclass for tv -> episodes and shows

--- tests/tv_tests.py
+++ tests/tv_tests.py
@@ -43,12 +43,15 @@ class TVShowTests(test.SickbeardTestDBCase):
         
         self.assertEqual(show.tvdbid, 0002)
         
-    def stest_change_name(self):
-        self.show.name = "newName"
-        self.show.saveToDB()
-        self.show.loadFromDB(skipNFO=True)
-        self.assertEqual(self.show.name, "newName")
-
+    def test_set_name(self):
+        show = TVShow(0001, "en")
+        show.name = "newName"
+        show.saveToDB()
+        show.loadFromDB(skipNFO=True)
+        self.assertEqual(show.name, "newName")
+        
+    
+        
 class TVEpisodeTests(test.SickbeardTestDBCase):
     
     def setUp(self):
@@ -58,7 +61,36 @@ class TVEpisodeTests(test.SickbeardTestDBCase):
     def test_init_empty_db(self):
         show = TVShow(0001, "en")
         self.assertRaises(exceptions.EpisodeNotFoundException, TVEpisode, show, test.SEASON, test.EPISODE)
+
+class TVTests(test.SickbeardTestDBCase):
     
+    def setUp(self):
+        super(TVEpisodeTests, self).setUp()
+        sickbeard.showList = []
+
+    def test_getEpisode(self):
+        show = TVShow(0001, "en")
+        show.name = "show name"
+        show.tvrname = "show name"
+        show.network = "cbs"
+        show.genre = "crime"
+        show.runtime = 40
+        show.status = "5"
+        show.airs = "monday"
+        show.startyear = 1987
+        show.saveToDB()
+        sickbeard.showList = [show]
+        
+        
+        
+        
+        pass 
+
+
+
+
+
+
 if __name__ == '__main__':
     print "=================="
     print "STARTING - TV TESTS"
@@ -68,6 +100,9 @@ if __name__ == '__main__':
     unittest.TextTestRunner(verbosity=2).run(suite)
     print "######################################################################"
     suite = unittest.TestLoader().loadTestsFromTestCase(TVEpisodeTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    print "######################################################################"
+    suite = unittest.TestLoader().loadTestsFromTestCase(TVTests)
     unittest.TextTestRunner(verbosity=2).run(suite)
     
 
