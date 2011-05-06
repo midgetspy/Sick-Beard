11b1771859029840a5e966b2c16917852ff3e16e
Dennis Lutter <lad1337@gmail.com>
2011-05-06 23:48:43 +0200

    when adding a show first load episodes from thetvdb then load files from dir

--- sickbeard/show_queue.py
+++ sickbeard/show_queue.py
@@ -274,12 +274,6 @@ class QueueItemAdd(ShowQueueItem):
         sickbeard.showList.append(self.show)
 
         try:
-            self.show.loadEpisodesFromDir()
-        except Exception, e:
-            logger.log(u"Error searching dir for episodes: " + ex(e), logger.ERROR)
-            logger.log(traceback.format_exc(), logger.DEBUG)
-
-        try:
             self.show.loadEpisodesFromTVDB()
             self.show.setTVRID()
 
@@ -288,6 +282,12 @@ class QueueItemAdd(ShowQueueItem):
             
         except Exception, e:
             logger.log(u"Error with TVDB, not creating episode list: " + ex(e), logger.ERROR)
+            logger.log(traceback.format_exc(), logger.DEBUG)
+
+        try:
+            self.show.loadEpisodesFromDir()
+        except Exception, e:
+            logger.log(u"Error searching dir for episodes: " + ex(e), logger.ERROR)
             logger.log(traceback.format_exc(), logger.DEBUG)
 
         try:
