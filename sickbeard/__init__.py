7b1c61c07c8575d082a3bf58f2abfd9a70c097f7
Daniel Pavel <daniel.pavel@gmail.com>
2011-05-17 00:51:56 +0300

    added support for torrent site freshon.tv rss feeds

--- sickbeard/__init__.py
+++ sickbeard/__init__.py
@@ -30,7 +30,7 @@ from threading import Lock
 
 # apparently py2exe won't build these unless they're imported somewhere
 from sickbeard import providers, metadata
-from providers import ezrss, tvtorrents, nzbs_org, nzbmatrix, nzbsrus, newznab, womble, newzbin
+from providers import ezrss, tvtorrents, freshontv, nzbs_org, nzbmatrix, nzbsrus, newznab, womble, newzbin
 
 from sickbeard import searchCurrent, searchBacklog, showUpdater, versionChecker, properFinder, autoPostProcesser
 from sickbeard import helpers, db, exceptions, show_queue, search_queue, scheduler
@@ -147,6 +147,8 @@ EZRSS = False
 TVTORRENTS = False
 TVTORRENTS_DIGEST = None
 TVTORRENTS_HASH = None
+FRESHONTV = False
+FRESHONTV_PASSKEY = None
 
 
 TORRENT_DIR = None
@@ -348,7 +350,7 @@ def initialize(consoleLogging=True):
                 USE_PLEX, PLEX_NOTIFY_ONSNATCH, PLEX_NOTIFY_ONDOWNLOAD, PLEX_UPDATE_LIBRARY, \
                 PLEX_SERVER_HOST, PLEX_HOST, PLEX_USERNAME, PLEX_PASSWORD, \
                 showUpdateScheduler, __INITIALIZED__, LAUNCH_BROWSER, showList, loadingShowList, \
-                NZBS, NZBS_UID, NZBS_HASH, EZRSS, TVTORRENTS, TVTORRENTS_DIGEST, TVTORRENTS_HASH, TORRENT_DIR, USENET_RETENTION, SOCKET_TIMEOUT, \
+                NZBS, NZBS_UID, NZBS_HASH, EZRSS, TVTORRENTS, TVTORRENTS_DIGEST, TVTORRENTS_HASH, FRESHONTV, FRESHONTV_PASSKEY, TORRENT_DIR, USENET_RETENTION, SOCKET_TIMEOUT, \
                 SEARCH_FREQUENCY, DEFAULT_SEARCH_FREQUENCY, BACKLOG_SEARCH_FREQUENCY, \
                 QUALITY_DEFAULT, SEASON_FOLDERS_FORMAT, SEASON_FOLDERS_DEFAULT, STATUS_DEFAULT, \
                 GROWL_NOTIFY_ONSNATCH, GROWL_NOTIFY_ONDOWNLOAD, TWITTER_NOTIFY_ONSNATCH, TWITTER_NOTIFY_ONDOWNLOAD, \
@@ -491,6 +493,8 @@ def initialize(consoleLogging=True):
         TVTORRENTS = bool(check_setting_int(CFG, 'TVTORRENTS', 'tvtorrents', 0))    
         TVTORRENTS_DIGEST = check_setting_str(CFG, 'TVTORRENTS', 'tvtorrents_digest', '')
         TVTORRENTS_HASH = check_setting_str(CFG, 'TVTORRENTS', 'tvtorrents_hash', '')
+        FRESHONTV = bool(check_setting_int(CFG, 'FRESHONTV', 'freshontv', 0))
+        FRESHONTV_PASSKEY = check_setting_str(CFG, 'FRESHONTV', 'freshontv_passkey', '')
 
         NZBS = bool(check_setting_int(CFG, 'NZBs', 'nzbs', 0))
         NZBS_UID = check_setting_str(CFG, 'NZBs', 'nzbs_uid', '')
@@ -973,6 +977,10 @@ def save_config():
     new_config['TVTORRENTS']['tvtorrents'] = int(TVTORRENTS)
     new_config['TVTORRENTS']['tvtorrents_digest'] = TVTORRENTS_DIGEST
     new_config['TVTORRENTS']['tvtorrents_hash'] = TVTORRENTS_HASH
+
+    new_config['FRESHONTV'] = {}
+    new_config['FRESHONTV']['freshontv'] = int(FRESHONTV)
+    new_config['FRESHONTV']['freshontv_passkey'] = FRESHONTV_PASSKEY
 
     new_config['NZBs'] = {}
     new_config['NZBs']['nzbs'] = int(NZBS)
---
+++ sickbeard/providers/freshontv.py
@@ -0,0 +1,81 @@
+# Author: Nic Wolfe <nic@wolfeden.ca>
+# URL: http://code.google.com/p/sickbeard/
+#
+# This file is part of Sick Beard.
+#
+# Sick Beard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Sick Beard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
+
+import xml.etree.cElementTree as etree
+
+import sickbeard
+import generic
+
+from sickbeard import logger
+from sickbeard import tvcache
+
+class FreshOnTvProvider(generic.TorrentProvider):
+
+    def __init__(self):
+
+        generic.TorrentProvider.__init__(self, "FreshOnTv")
+        
+        self.supportsBacklog = False
+
+        self.cache = FreshOnTVCache(self)
+
+        self.url = 'http://freshon.tv/'
+
+    def isEnabled(self):
+        return sickbeard.FRESHONTV
+        
+    def imageName(self):
+        return 'freshontv.gif'
+
+class FreshOnTVCache(tvcache.TVCache):
+
+    def __init__(self, provider):
+
+        tvcache.TVCache.__init__(self, provider)
+
+        # only poll TvTorrents every 15 minutes max
+        self.minTime = 15
+
+
+    def _getRSSData(self):
+        # These will be ignored on the serverside.
+        ignore_regex = "all.month|month.of|season[\s\d]*complete"
+    
+        url = 'http://freshon.tv/rss.php?feed=dl&passkey='+ sickbeard.FRESHONTV_PASSKEY
+        logger.log(u"FreshOn.tv cache update URL: "+ url, logger.DEBUG)
+
+        data = self.provider.getURL(url)
+        if not data:
+            logger.log(u"FreshOn.tv invalid passkey, check your config", logger.ERROR)
+
+        return data
+
+    def _parseItem(self, item):
+
+        title = item.findtext('title')
+        url = item.findtext('link')
+
+        if not title or not url:
+            logger.log(u"The XML returned from the FreshOn.tv RSS feed is incomplete, this result is unusable", logger.ERROR)
+            return
+
+        logger.log(u"Adding item from RSS to cache: "+title, logger.DEBUG)
+
+        self._addCacheEntry(title, url)
+
+provider = FreshOnTvProvider()
