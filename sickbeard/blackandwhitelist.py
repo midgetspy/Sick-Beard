e722aa14e13be2e8273651b743c586369122eadc
Dennis Lutter <lad1337@gmail.com>
2011-06-20 11:10:35 +0200

    setter for black and white list

--- sickbeard/blackandwhitelist.py
+++ sickbeard/blackandwhitelist.py
@@ -19,6 +19,8 @@
 from sickbeard import db, logger
 
 class BlackAndWhiteList(object):
+    _tabeBlack = "blacklist"
+    _tabeWhite = "whitelist"
     
     def __init__(self,show_id):
         if not show_id:
@@ -30,32 +32,82 @@ class BlackAndWhiteList(object):
     def refresh(self):                
         logger.log(u"Building black and white list for "+str(self.show_id), logger.DEBUG)
 
-        self.blacklist = self.load_blacklist()
-        self.whitelist = self.load_whitelist()
+        (self.blackList,self.blackDict) = self.load_blacklist()
+        (self.whiteList,self.whiteDict) = self.load_whitelist()
 
     def load_blacklist(self):
-        return self._load_list("blacklist")
+        return self._load_list(self._tabeBlack)
     
     def load_whitelist(self):
-        return self._load_list("whitelist")    
+        return self._load_list(self._tabeWhite)    
+    
+    def set_black_keywords(self,range,values):
+        self._del_all_black_keywors()
+        self._add_keywords(self._tabeBlack, range, values)
+
+    def set_white_keywords(self,range,values):
+        self._del_all_white_keywors()
+        self._add_keywords(self._tabeWhite, range, values)
+  
+    def set_black_keywords_for(self,range,values):
+        self._del_all_black_keywors_for(range)
+        self._add_keywords(self._tabeBlack, range, values)
+
+    def set_white_keywords_for(self,range,values):
+        self._del_all_white_keywors_for(range)
+        self._add_keywords(self._tabeWhite, range, values)
+    
+    def add_black_keyword(self,range,value):
+        self._add_keywords(self._tabeBlack, range, [value])
+        
+    def add_white_keyword(self,range,value):
+        self._add_keywords(self._tabeWhite, range, [value])
+    
+    def _add_keywords(self,table,range,values):
+        for value in values:
+            self.myDB.action("INSERT INTO "+table+" show_id, range , keyword VALUES ?,?,?", [self.show_id,range,value])        
+    
+    def _del_all_black_keywors(self):
+        self._del_all_keywords(self._tabeBlack)
+    
+    def _del_all_white_keywors(self):
+        self._del_all_keywords(self._tabeWhite)
+        
+    def _del_all_black_keywors_for(self,range):
+        self._del_all_keywords_for(self._tabeBlack,range)
+    
+    def _del_all_white_keywors_for(self,range):
+        self._del_all_keywords_for(self._tabeWhite.range)
+    
+    def _del_all_keywords(self,table):
+        logger.log(u"Deleting all "+table+" keywords for "+str(self.show_id), logger.DEBUG)
+        self.myDB.action("DELETE FROM "+table+" WHERE show_id = ?", [self.show_id])
+    
+    def _del_all_keywords_for(self,table,range):
+        logger.log(u"Deleting all "+range+" "+table+" keywords for "+str(self.show_id), logger.DEBUG)
+        self.myDB.action("DELETE FROM "+table+" WHERE show_id = ? and range = ?", [self.show_id,range])
     
     def _load_list(self,table):
         sqlResults = self.myDB.select("SELECT range,keyword FROM "+table+" WHERE show_id = ? ", [self.show_id])
         if not sqlResults or not len(sqlResults):
-            return []
+            return ([],{})
         
         return self._build_keyword_dict(sqlResults)
     
     def _build_keyword_dict(self,sql_result):
         list = []
+        dict = {}
         for row in sql_result:
-            list.append(BlackWhiteKeyword(row["range"],row["keyword"]))
-        return list
+            list.append(BlackWhiteKeyword(row["range"],[row["keyword"]]))
+            dict[row["range"]].update(row["keyword"])
+        for range in dict:
+            dict[range] = BlackWhiteKeyword(range,dict[range])
+        return (list,dict)
     
 class BlackWhiteKeyword(object):
-    def __init__(self, range, value):
-        self.range = range
-        self.value = value
+    def __init__(self, range, values):
+        self.range = range # "global" or a parser group
+        self.value = values # a list of values may contain onyl one item (still a list)
         
 
 class BlackWhitelistNoShowIDException(Exception):
