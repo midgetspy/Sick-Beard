ba8f3988fa738a8ccf37d45140b34d1b2a51bc73
Dennis Lutter <lad1337@gmail.com>
2011-07-07 20:37:45 +0200

    initial anidb unittests

---
+++ tests/anidb_tests.py
@@ -0,0 +1,34 @@
+# coding=UTF-8
+import unittest
+import test_lib as test
+
+import sys, os.path
+sys.path.append(os.path.abspath('..'))
+sys.path.append(os.path.abspath('../lib'))
+import sickbeard
+from sickbeard.helpers import *
+    
+class AnidbBasicTests(test.SickbeardTestConfigCase):
+    
+    def setUp(self):
+        tmpConnection = sickbeard.ADBA_CONNECTION
+        super(AnidbBasicTests, self).setUp()
+        sickbeard.ADBA_CONNECTION = tmpConnection
+        
+        
+    def test_createConnection(self):
+        self.assertTrue(set_up_anidb_connection());
+    
+    def test_lookup(self):
+        if set_up_anidb_connection():
+            pass
+        else:
+            self.fail("Can not setup a connection")
+    
+if __name__ == '__main__':
+    print "=================="
+    print "STARTING - ANIDB TESTS"
+    print "=================="
+    print "######################################################################"
+    suite = unittest.TestLoader().loadTestsFromTestCase(AnidbBasicTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
