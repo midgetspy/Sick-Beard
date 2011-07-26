3f4e732a79709388a301a6b5e27fb21ee0ba5155
Jonathon Saine <thezoggy@gmail.com>
2011-07-26 14:40:11 -0500

    Changed history limit so that 0 is 'all', that way we can just use int's instead of having to code for string + int checking. Corrected some minor typos. Changed variable names to be more consistent to match other functions. Removed extra whitespace.

--- sickbeard/webapi.py
+++ sickbeard/webapi.py
@@ -35,7 +35,6 @@ dateFormat = ""
 dayofWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
  
 class Api:
-    
     """
         generic api will always return json
     """
@@ -44,8 +43,7 @@ class Api:
 
     @cherrypy.expose
     def default(self, *args, **kwargs):
-        
-        
+
         self.apiKey = "1234"
         access,accessMsg,args,kwargs = self._grand_access(self.apiKey,args,kwargs)
 
@@ -59,7 +57,7 @@ class Api:
         else:
             logger.log(accessMsg,logger.WARNING)
             return outputCallback(_error(accessMsg))
-        
+
         # global dateForm
         # TODO: refactor dont change the module var all the time
         global dateFormat
@@ -68,7 +66,7 @@ class Api:
             dateFormat = str(kwargs["dateForm"])
             del kwargs["dateForm"]
 
-        
+
         # set the original call_dispatcher as the local _call_dispatcher
         _call_dispatcher = call_dispatcher
         # if profile was set wrap "_call_dispatcher" in the profile function
@@ -89,7 +87,8 @@ class Api:
                 outDict = _error(ex(e))
 
         return outputCallback(outDict)
-    
+
+
     def _out_as_json(self,dict):
         response = cherrypy.response
         response.headers['Content-Type'] = 'application/json;charset=UTF-8'
@@ -98,12 +97,11 @@ class Api:
         except Exception, e: # if we fail to generate the output fake a error
             out = '{"error": "while composing output: "'+ex(e)+'"}'
         return out
-    
-  
+
+
     def _grand_access(self,realKey,args,kwargs):
         remoteIp = cherrypy.request.remote.ip
-        
-        
+
         apiKey = kwargs.get("apikey",None)
 
         if not apiKey:
@@ -116,7 +114,7 @@ class Api:
         else:
             del kwargs["apikey"]
 
-     
+
         if apiKey == realKey:
             msg = u"Api key accepted. ACCESS GRANTED"
             return True, msg, args, kwargs
@@ -126,34 +124,32 @@ class Api:
         else:
             msg = u"Api key '"+apiKey+"' given by '"+remoteIp+"' NOT accepted. ACCESS DENIED"
             return False, msg, args, kwargs
-        
+
 
 def call_dispatcher(args, kwargs):
-    
     logger.log("api: all args: '"+str(args)+"'",logger.DEBUG)
     logger.log("api: all kwargs: '"+str(kwargs)+"'",logger.DEBUG)
     logger.log("api: dateFormat: '"+str(dateFormat)+"'",logger.DEBUG)
-    
+
     cmd = None
     if args:
         cmd = args[0]
         args = args[1:]
-        
+
     if kwargs.get("cmd"):
         cmd = kwargs.get("cmd")
-    
+
     if _functionMaper.get(cmd, False):
         outDict = _functionMaper.get(cmd)(args,kwargs)
     elif _is_int(cmd):
         outDict = id_url_wrapper(cmd,args,kwargs)        
     else:
         outDict = getIndex(args,kwargs)
-    
+
     return outDict
 
 
 def getIndex(args,kwargs):
-    
     return {'sb_version': sickbeard.version.SICKBEARD_VERSION, 'api_version':Api.version}
 
 
@@ -166,14 +162,13 @@ def getShows(args,kwargs):
     # one args element is gone so the next one should be paused
     paused,args,missing = _check_params(args, kwargs, "paused", None, missing)
     # you will see this pattern in all function that can called directly
-    
-    
-    
+
+
     shows = {}
     for show in sickbeard.showList:
         if paused and not paused == str(show.paused):
             continue
-        
+
         showDict = {"paused":show.paused,"quality":_get_quality_string(show.quality),"language":show.lang}
         if order == "name":
             showDict["tvdbid"] = show.tvdbid
@@ -181,7 +176,7 @@ def getShows(args,kwargs):
         else:
             showDict["name"] = show.name
             shows[show.tvdbid] = showDict
-    
+
     return shows
 
 
@@ -191,7 +186,7 @@ def getShow(args, kwargs):
         return _missing_param(missing)
     season,args,missing = _check_params(args, kwargs, "season", None)
     status,args,missing = _check_params(args, kwargs, "status", [])
-    
+
 
     show = sickbeard.helpers.findCertainShow(sickbeard.showList, int(tvdbid))
     if not show:
@@ -204,10 +199,10 @@ def getShow(args, kwargs):
         showDict["episodes"] = _episodes
     else:
         _episodes,episodeCounts,sList = _get_episodes(tvdbid)
-    
+
     showDict["season_list"] = sList
     showDict["stats"] = episodeCounts
-    
+
     genreList = []
     if show.genre:
         genreListTmp = show.genre.split("|")
@@ -216,7 +211,7 @@ def getShow(args, kwargs):
                 genreList.append(genre)
     showDict["genre"] = genreList
     showDict["quality"] = _get_quality_string(show.quality)
-    
+
     try:
         showDict["location"] = show.location
     except:
@@ -229,7 +224,7 @@ def getShow(args, kwargs):
     showDict["air_by_date"] = show.air_by_date
     showDict["seasonfolders"] = show.seasonfolders
     showDict["airs"] = show.airs
-    
+
     return showDict
 
 
@@ -249,15 +244,14 @@ def getEpisodes(args, kwargs):
     if missing:
         return _missing_param(missing)
     status,args,missing = _check_params(args, kwargs, "status", [])
-    
+
     if status:
         status = status.split(",")
     episodes,episodes_stats, season_list = _get_episodes(tvdbid, season=s, status=status)
-    
-    return episodes
-    
 
- 
+    return episodes
+
+
 def getEpisode(args, kwargs):
     tvdbid,args,missing = _check_params(args, kwargs, "tvdbid", None, [])
     s,args,missing = _check_params(args, kwargs, "s", None, missing)
@@ -268,25 +262,26 @@ def getEpisode(args, kwargs):
 
     if s == "all":
         s = None
-    
+
     _is_int_multi(tvdbid,e,s)
-    
+
     show = sickbeard.helpers.findCertainShow(sickbeard.showList, int(tvdbid))
-    
+
     myDB = db.DBConnection(row_type="dict")
+    #sqlResults = myDB.select( "SELECT showid AS 'tvdbid', tvdbid AS 'ep_tvdbid', name, season, episode, description, airdate AS 'ep_airdate', status, location FROM tv_episodes WHERE showid = ? AND episode = ? AND season = ?", [tvdbid,e,s])
     sqlResults = myDB.select( "SELECT * FROM tv_episodes WHERE showid = ? AND episode = ? AND season = ?", [tvdbid,e,s])
     episode = {}
     if len(sqlResults) == 1:
         episode = _make_episode_nice(sqlResults[0])
     else:
         raise ApiError("No episode found")
-    
+
     # delete unneeded info
     del episode["hastbn"]
     del episode["hasnfo"]
     del episode["episode_id"]
     del episode["tvdbid"]
-    
+
     # handle path options
     # absolute vs relative vs broken
     showPath = None
@@ -294,7 +289,7 @@ def getEpisode(args, kwargs):
         showPath = show.location
     except:
         pass
-    
+
     if fullPath == "1" and showPath: # we get the full path by default so no need to change
         pass 
     elif fullPath and showPath:
@@ -306,22 +301,21 @@ def getEpisode(args, kwargs):
     
     # rename
     episode = _rename_element(episode,"showid","tvdbid")
-    
-    
+
 
     return episode
 
-def getCommingEpisodes(args,kwargs):
+def getComingEpisodes(args,kwargs):
     sort,args,missing = _check_params(args, kwargs, "sort", "date")
-    
+
     if not sort in ["date","show","network"]:
         return _error("Sort by '"+sort+"' not possible")
-    
+
     epResults,today,next_week = webserve.WebInterface.commingEpisodesRaw(sort=sort ,row_type="dict")
     finalEpResults = []
     for ep in epResults:
         """
-            Missed:   yesterday... i don't know how far back we go
+            Missed:   yesterday... (less than 1week)
             Today:    today
             Soon:     tomorrow till next week
             Later:    later than next week
@@ -350,18 +344,16 @@ def getCommingEpisodes(args,kwargs):
         del ep["seasonfolders"]
         
         finalEpResults.append(_remove_clutter(ep))
-        
-       
+
+
     return finalEpResults
 
 
 def getHistory(args,kwargs):
     limit,args,missing = _check_params(args, kwargs, "limit", 100)
     type,args,missing = _check_params(args, kwargs, "type", None)
-    
-    if limit == "all":
-        limit = None
-    _is_int_multi(limit)
+
+    #_is_int_multi(limit)
 
     if type == "downloaded":
         type = "Downloaded"
@@ -369,14 +361,15 @@ def getHistory(args,kwargs):
         type = "Snatched"
 
     myDB = db.DBConnection(row_type="dict")
-    
-    if limit:
-        hResults = myDB.select("SELECT h.*, show_name FROM history h, tv_shows s WHERE h.showid=s.tvdb_id ORDER BY date DESC LIMIT ?",[limit] )
-    else:
-        hResults = myDB.select("SELECT h.*, show_name FROM history h, tv_shows s WHERE h.showid=s.tvdb_id ORDER BY date DESC")
 
-    finalHresults = []
-    for row in hResults:
+    ulimit = min(int(limit), 100)
+    if ulimit == 0:
+        sqlResults = myDB.select("SELECT h.*, show_name FROM history h, tv_shows s WHERE h.showid=s.tvdb_id ORDER BY date DESC" )
+    else:
+        sqlResults = myDB.select("SELECT h.*, show_name FROM history h, tv_shows s WHERE h.showid=s.tvdb_id ORDER BY date DESC LIMIT ?",[ ulimit ] )
+
+    results = []
+    for row in sqlResults:
         status, quality = Quality.splitCompositeStatus(int(row["action"]))
         status = _get_status_Strings(status)
         if type and not status == type:
@@ -385,8 +378,8 @@ def getHistory(args,kwargs):
         row["quality"] = _get_quality_string(quality)
         row["date"] = _historyDate_to_dateForm(str(row["date"]))
         _rename_element(row, "showid", "tvdbid")
-        finalHresults.append(row)
-    return finalHresults
+        results.append(row)
+    return results
 
 
 def id_url_wrapper(sid,args,kwargs):
@@ -410,6 +403,7 @@ def id_url_wrapper(sid,args,kwargs):
     else:
         return getShow(args, kwargs)
 
+
 def _is_int(foo):
     try:
         int(foo)
@@ -424,6 +418,7 @@ def _is_int_multi(*vars):
             raise IntParseError("'" +var + "' is not parsable into a int, but is supposed to be. Canceling")
     return True
 
+
 def _rename_element(dict,oldKey,newKey):
     try:
         dict[newKey] = dict[oldKey]
@@ -432,8 +427,10 @@ def _rename_element(dict,oldKey,newKey):
         pass
     return dict
 
+
 def _error(msg):
     return {'error':msg}
+
 
 def _get_quality_string(q):
     qualityString = "Custom"
@@ -442,6 +439,7 @@ def _get_quality_string(q):
     elif q in Quality.qualityStrings:
         qualityString = Quality.qualityStrings[q]
     return qualityString
+
 
 def _get_status_Strings(s):
     return statusStrings[s]
@@ -453,42 +451,44 @@ def _missing_param(missingList):
         msg = "The required parameters: " + ",".join(missingList) +" where not set"
     return _error(msg)
 
-def _get_episodes(showId,season=None,status=[]):
+
+def _get_episodes(tvdbid,season=None,status=[]):
     """
         create a dictonary containing episode dicts
         if season only given season will be returned 
         if pure there is no season container around the episodes
         if status only gets episodes that have the a status in status
     """
-    
+
     pure = False
     if season == "all":
         season = None
 
     _is_int_multi(season)
-        
+
     if season:
         season = int(season)
         pure = True
-    
+
     status = _replace_statusStrings_with_statusCodes(status)
-    
+
 
     myDB = db.DBConnection(row_type="dict")
-    sqlResults = myDB.select( "SELECT * FROM tv_episodes WHERE showid = ? ORDER BY season*1000+episode DESC", [showId])
+    #sqlResults = myDB.select( "SELECT episode_id, tvdbid AS 'ep_tvdbid', name, season, episode, airdate AS 'ep_airdate', status FROM tv_episodes WHERE showid = ? ORDER BY season*1000+episode DESC", [tvdbid])
+    sqlResults = myDB.select( "SELECT * FROM tv_episodes WHERE showid = ? ORDER BY season*1000+episode DESC", [tvdbid])
     episodes = {} # will contain all episodes that selected
-    
+
     # show stats
     all_count = 0 # all episodes
     loaded_count = 0
     snatched_count = 0
     season_list = [] # a list with all season numbers
-    
+
     episode_status_counts = {}
     # add the default status
     for statusString in statusStrings.statusStrings:
         episode_status_counts[statusStrings[statusString]] = 0
-    
+
     # loop through ALL episodes of show
     for epResult in sqlResults:
         
@@ -499,18 +499,18 @@ def _get_episodes(showId,season=None,status=[]):
                 loaded_count += 1
             elif epResult["status"] in Quality.SNATCHED:
                 snatched_count += 1
-        
+
         # skip episodes that dont match the status filter
         if status and not epResult["status"] in status:
             continue
 
         epResult = _make_episode_nice(epResult)
-        
+
         # add the specific status like "Downloaded (HDTV)"
         if not episode_status_counts.has_key(epResult["status"]):
             episode_status_counts[epResult["status"]] = 0
         episode_status_counts[epResult["status"]] += 1
-            
+
         if season and season != curSeason:
             continue
 
@@ -539,25 +539,30 @@ def _get_episodes(showId,season=None,status=[]):
     episodes_stats["Snatched"] = loaded_count
 
     return episodes, episodes_stats, season_list
-    
+
+
 def _make_episode_nice(epResult):
     epResult["status"] = statusStrings[epResult["status"]]
     epResult["airdate"] = _ordinal_to_dateForm(int(epResult["airdate"]))
-    
+
     return epResult
+
 
 def _ordinal_to_dateForm(ordinal):
     date = datetime.date.fromordinal(ordinal)
     return _convert_date_dateform(date,ordinal)
 
+
 def _historyDate_to_dateForm(timeString):
     date = datetime.datetime.strptime(timeString,history.dateFormat)
     return _convert_date_dateform(date,timeString)
-    
+
+
 def _convert_date_dateform(date,raw):
     if not dateFormat or dateFormat == "raw":
         return raw
     return date.strftime(dateFormat)
+
 
 def _remove_clutter(epResult):
     del epResult["showid"]
@@ -566,11 +571,12 @@ def _remove_clutter(epResult):
     del epResult["hastbn"]
     del epResult["hasnfo"]
     del epResult["episode_id"]
-    
+
     del epResult["episode"]
     del epResult["season"]
-    
+
     return epResult
+
 
 def _replace_statusStrings_with_statusCodes(statusStrings):
     statusCodes = []
@@ -589,6 +595,7 @@ def _replace_statusStrings_with_statusCodes(statusStrings):
     if "unaired" in statusStrings:
         statusCodes.append(UNAIRED)
     return statusCodes
+
 
 def _check_params(args,kwargs,key,default,missingList=[],remove=True):
     """
@@ -621,7 +628,7 @@ _functionMaper = {"index":getIndex,
                   "episodes":getEpisodes,
                   "episode":getEpisode,
                   "season":getSeason,
-                  "future":getCommingEpisodes,
+                  "future":getComingEpisodes,
                   "history":getHistory,
                   }
 
