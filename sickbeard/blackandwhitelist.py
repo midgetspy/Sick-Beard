bb39f7fc4e6f6c7756aeb8593d80a08755eb9ad0
Dennis Lutter <lad1337@gmail.com>
2011-08-22 15:08:48 +0200

    pep8

--- sickbeard/blackandwhitelist.py
+++ sickbeard/blackandwhitelist.py
@@ -25,120 +25,120 @@ class BlackAndWhiteList(object):
     whiteList = []
     blackDict = {}
     whiteDict = {}
-    
-    def __init__(self,show_id):
+
+    def __init__(self, show_id):
         if not show_id:
             raise BlackWhitelistNoShowIDException()
         self.show_id = show_id
         self.myDB = db.DBConnection()
         self.refresh()
-       
-    def refresh(self):                
-        logger.log(u"Building black and white list for "+str(self.show_id), logger.DEBUG)
 
-        (self.blackList,self.blackDict) = self.load_blacklist()
-        (self.whiteList,self.whiteDict) = self.load_whitelist()
+    def refresh(self):
+        logger.log(u"Building black and white list for " + str(self.show_id), logger.DEBUG)
+
+        (self.blackList, self.blackDict) = self.load_blacklist()
+        (self.whiteList, self.whiteDict) = self.load_whitelist()
 
     def load_blacklist(self):
         return self._load_list(self._tableBlack)
-    
+
     def load_whitelist(self):
-        return self._load_list(self._tableWhite)    
-    
-    def get_black_keywords_for(self,range):
-        if self.blackDict.has_key(range):
+        return self._load_list(self._tableWhite)
+
+    def get_black_keywords_for(self, range):
+        if range in self.blackDict:
             return self.blackDict[range]
         else:
             return []
-    
-    def get_white_keywords_for(self,range):
-        if self.whiteDict.has_key(range):
+
+    def get_white_keywords_for(self, range):
+        if range in self.whiteDict:
             return self.whiteDict[range]
         else:
             return []
 
-    def set_black_keywords(self,range,values):
+    def set_black_keywords(self, range, values):
         self._del_all_black_keywors()
         self._add_keywords(self._tableBlack, range, values)
 
-    def set_white_keywords(self,range,values):
+    def set_white_keywords(self, range, values):
         self._del_all_white_keywors()
         self._add_keywords(self._tableWhite, range, values)
-  
-    def set_black_keywords_for(self,range,values):
+
+    def set_black_keywords_for(self, range, values):
         self._del_all_black_keywors_for(range)
         self._add_keywords(self._tableBlack, range, values)
 
-    def set_white_keywords_for(self,range,values):
+    def set_white_keywords_for(self, range, values):
         self._del_all_white_keywors_for(range)
         self._add_keywords(self._tableWhite, range, values)
-    
-    def add_black_keyword(self,range,value):
+
+    def add_black_keyword(self, range, value):
         self._add_keywords(self._tableBlack, range, [value])
-        
-    def add_white_keyword(self,range,value):
+
+    def add_white_keyword(self, range, value):
         self._add_keywords(self._tableWhite, range, [value])
-    
-    def _add_keywords(self,table,range,values):
+
+    def _add_keywords(self, table, range, values):
         for value in values:
-            self.myDB.action("INSERT INTO "+table+" (show_id, range , keyword) VALUES (?,?,?)", [self.show_id,range,value])        
+            self.myDB.action("INSERT INTO " + table + " (show_id, range , keyword) VALUES (?,?,?)", [self.show_id, range, value])
         self.refresh()
-        
+
     def _del_all_black_keywors(self):
         self._del_all_keywords(self._tableBlack)
-    
+
     def _del_all_white_keywors(self):
         self._del_all_keywords(self._tableWhite)
-        
-    def _del_all_black_keywors_for(self,range):
-        self._del_all_keywords_for(self._tableBlack,range)
-    
-    def _del_all_white_keywors_for(self,range):
-        self._del_all_keywords_for(self._tableWhite,range)
-    
-    def _del_all_keywords(self,table):
-        logger.log(u"Deleting all "+table+" keywords for "+str(self.show_id), logger.DEBUG)
-        self.myDB.action("DELETE FROM "+table+" WHERE show_id = ?", [self.show_id])
+
+    def _del_all_black_keywors_for(self, range):
+        self._del_all_keywords_for(self._tableBlack, range)
+
+    def _del_all_white_keywors_for(self, range):
+        self._del_all_keywords_for(self._tableWhite, range)
+
+    def _del_all_keywords(self, table):
+        logger.log(u"Deleting all " + table + " keywords for " + str(self.show_id), logger.DEBUG)
+        self.myDB.action("DELETE FROM " + table + " WHERE show_id = ?", [self.show_id])
         self.refresh()
-    
-    def _del_all_keywords_for(self,table,range):
-        logger.log(u"Deleting all "+range+" "+table+" keywords for "+str(self.show_id), logger.DEBUG)
-        self.myDB.action("DELETE FROM "+table+" WHERE show_id = ? and range = ?", [self.show_id,range])
+
+    def _del_all_keywords_for(self, table, range):
+        logger.log(u"Deleting all " + range + " " + table + " keywords for " + str(self.show_id), logger.DEBUG)
+        self.myDB.action("DELETE FROM " + table + " WHERE show_id = ? and range = ?", [self.show_id, range])
         self.refresh()
-        
-    def _load_list(self,table):
-        sqlResults = self.myDB.select("SELECT range,keyword FROM "+table+" WHERE show_id = ? ", [self.show_id])
+
+    def _load_list(self, table):
+        sqlResults = self.myDB.select("SELECT range,keyword FROM " + table + " WHERE show_id = ? ", [self.show_id])
         if not sqlResults or not len(sqlResults):
-            return ([],{})
-        
+            return ([], {})
+
         list, dict = self._build_keyword_dict(sqlResults)
-        logger.log("BWL: "+str(self.show_id)+" loaded keywords from "+table+": "+str(dict), logger.DEBUG)
+        logger.log("BWL: " + str(self.show_id) + " loaded keywords from " + table + ": " + str(dict), logger.DEBUG)
         return list, dict
-    
-    def _build_keyword_dict(self,sql_result):
+
+    def _build_keyword_dict(self, sql_result):
         list = []
         dict = {}
         for row in sql_result:
             list.append(row["keyword"])
-            if(dict.has_key(row["range"])):
+            if row["range"] in dict:
                 dict[row["range"]].append(row["keyword"])
             else:
                 dict[row["range"]] = [row["keyword"]]
-        
-        return (list,dict)
-    
-    def is_valid_for_black(self,haystack):
-        logger.log(u"BWL: "+str(self.show_id)+" is valid black", logger.DEBUG)
+
+        return (list, dict)
+
+    def is_valid_for_black(self, haystack):
+        logger.log(u"BWL: " + str(self.show_id) + " is valid black", logger.DEBUG)
         return self._is_valid_for(self.blackDict, False, haystack)
 
-    def is_valid_for_white(self,haystack):
-        logger.log(u"BWL: "+str(self.show_id)+" is valid white", logger.DEBUG)
+    def is_valid_for_white(self, haystack):
+        logger.log(u"BWL: " + str(self.show_id) + " is valid white", logger.DEBUG)
         return self._is_valid_for(self.whiteDict, True, haystack)
 
-    def is_valid(self,haystack):
+    def is_valid(self, haystack):
         return self.is_valid_for_black(haystack) and self.is_valid_for_white(haystack)
-    
-    def _is_valid_for(self,list,mood,haystack):
+
+    def _is_valid_for(self, list, mood, haystack):
         if not len(list):
             return True
 
@@ -148,13 +148,13 @@ class BlackAndWhiteList(object):
                 string = None
                 if range == "global":
                     string = haystack.name
-                elif haystack.__dict__.has_key(range):
+                elif range in haystack.__dict__:
                     string = haystack.__dict__[range]
-                elif not haystack.__dict__.has_key(range):
+                elif not range in haystack.__dict__:
                     results.append((not mood))
                 else:
                     results.append(False)
-                
+
                 if string:
                     results.append(self._is_keyword_in_string(string, keyword) == mood)
 
@@ -164,24 +164,25 @@ class BlackAndWhiteList(object):
             return mood
         else:
             return (not mood)
-    
-    def _is_keyword_in_string(self,fromPost,fromBWList):
+
+    def _is_keyword_in_string(self, fromPost, fromBWList):
         """
         will return true if fromBWList is found in fromPost
         for now a basic find is used
         """
         fromPost = fromPost.lower()
         fromBWList = fromBWList.lower()
-        logger.log(u"BWL: "+str(self.show_id)+" comparing fromPost: "+fromPost+" vs fromBWlist: "+fromBWList, logger.DEBUG)
+        logger.log(u"BWL: " + str(self.show_id) + " comparing fromPost: " + fromPost + " vs fromBWlist: " + fromBWList, logger.DEBUG)
         return (fromPost.find(fromBWList) >= 0)
 
 class BlackWhiteKeyword(object):
     range = ""
     value = []
+
     def __init__(self, range, values):
         self.range = range # "global" or a parser group
         self.value = values # a list of values may contain only one item (still a list)
-        
+
 
 class BlackWhitelistNoShowIDException(Exception):
     "No show_id was given"
