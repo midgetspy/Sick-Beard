6033573282d4a99ee2ac41d8dc77c2e5fb2d9dc5
Dennis Lutter <lad1337@gmail.com>
2011-09-09 10:22:33 +0200

    pep8, pylint (all above 9), and license block i took the liberty to use my name as author

--- tests/pp_tests.py
+++ tests/pp_tests.py
@@ -1,4 +1,22 @@
 # coding=UTF-8
+# Author: Dennis Lutter <lad1337@gmail.com>
+# URL: http://code.google.com/p/sickbeard/
+#
+# This file is part of Sick Beard.
+#
+# Sick Beard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Sick Beard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
+
 import random
 import unittest
 
@@ -11,16 +29,16 @@ sys.path.append(os.path.abspath('../lib'))
 
 from sickbeard.postProcessor import PostProcessor
 import sickbeard
-from sickbeard.tv import TVEpisode,TVShow
+from sickbeard.tv import TVEpisode, TVShow
 
 class PPInitTests(unittest.TestCase):
 
     def setUp(self):
         self.pp = PostProcessor(test.FILEPATH)
-        
+
     def test_init_file_name(self):
         self.assertEqual(self.pp.file_name, test.FILENAME)
- 
+
     def test_init_folder_name(self):
         self.assertEqual(self.pp.folder_name, test.SHOWNAME)
 
@@ -29,12 +47,12 @@ class PPPrivateTests(test.SickbeardTestDBCase):
 
     def setUp(self):
         super(PPPrivateTests, self).setUp()
-        
-        sickbeard.showList = [TVShow(0000),TVShow(0001)]
-        
+
+        sickbeard.showList = [TVShow(0000), TVShow(0001)]
+
         self.pp = PostProcessor(test.FILEPATH)
         self.show_obj = TVShow(0002)
-        
+
         self.db = test.db.DBConnection()
         newValueDict = {"tvdbid": 1002,
                         "name": test.SHOWNAME,
@@ -50,23 +68,23 @@ class PPPrivateTests(test.SickbeardTestDBCase):
 
         # use a custom update/insert method to get the data into the DB
         self.db.upsert("tv_episodes", newValueDict, controlValueDict)
-        
+
         self.ep_obj = TVEpisode(self.show_obj, test.SEASON, test.EPISODE, test.FILEPATH)
-    
+
     def test__find_ep_destination_folder(self):
         self.show_obj.location = test.FILEDIR
         self.ep_obj.show.seasonfolders = 1
         sickbeard.SEASON_FOLDERS_FORMAT = 'Season %02d'
         calculatedPath = self.pp._find_ep_destination_folder(self.ep_obj)
-        ecpectedPath = os.path.join(test.FILEDIR, "Season 0"+str(test.SEASON))
-        self.assertEqual(calculatedPath,ecpectedPath)
+        ecpectedPath = os.path.join(test.FILEDIR, "Season 0" + str(test.SEASON))
+        self.assertEqual(calculatedPath, ecpectedPath)
 
 
 class PPBasicTests(test.SickbeardTestDBCase):
     def setUp(self):
         super(PPBasicTests, self).setUp()
         self.pp = PostProcessor(test.FILEPATH)
-    
+
     @unittest.skip("this test is not fully configured / implmented")
     def test_process(self):
         self.assertTrue(self.pp.process())
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,3 +1,22 @@
+# coding=UTF-8
+# Author: Dennis Lutter <lad1337@gmail.com>
+# URL: http://code.google.com/p/sickbeard/
+#
+# This file is part of Sick Beard.
+#
+# Sick Beard is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Sick Beard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
+
 import unittest
 
 import sqlite3
@@ -7,8 +26,8 @@ sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('../lib'))
 
 import sickbeard
-import shutil, time, os.path, sys
-from sickbeard import encodingKludge as ek 
+import shutil, time
+from sickbeard import encodingKludge as ek
 from sickbeard import db
 from sickbeard.databases import mainDB
 
@@ -37,9 +56,9 @@ TESTDBNAME = "test_sickbeard.db"
 SHOWNAME = u"show name"
 SEASON = 4
 EPISODE = 2
-FILENAME = u"show name - s0"+str(SEASON)+"e0"+str(EPISODE)+".mkv"
+FILENAME = u"show name - s0" + str(SEASON) + "e0" + str(EPISODE) + ".mkv"
 FILEDIR = os.path.join(TESTDIR, SHOWNAME)
-FILEPATH = os.path.join(FILEDIR,FILENAME)
+FILEPATH = os.path.join(FILEDIR, FILENAME)
 
 #=================
 # dummy functions
@@ -59,17 +78,17 @@ class SickbeardTestDBCase(unittest.TestCase):
         sickbeard.showList = []
         setUp_test_db()
         setUp_test_episode_file()
-    
+
     def tearDown(self):
         sickbeard.showList = []
-        tearDown_test_db() 
+        tearDown_test_db()
         tearDown_test_episode_file()
 
-class TestDBConnection(db.DBConnection,object):
+class TestDBConnection(db.DBConnection, object):
     def __init__(self, dbFileName=TESTDBNAME):
         dbFileName = os.path.join(TESTDIR, dbFileName)
         super(TestDBConnection, self).__init__(dbFileName)
-        
+
 # this will override the normal db connection
 sickbeard.db.DBConnection = TestDBConnection
 
@@ -84,8 +103,8 @@ def setUp_test_db():
     db.upgradeDatabase(db.DBConnection(), mainDB.InitialSchema)
     # fix up any db problems
     db.sanityCheckDatabase(db.DBConnection(), mainDB.MainSanityCheck)
-    
-       
+
+
 def tearDown_test_db():
     """Deletes the test db
         although this seams not to work on my system it leaves me with an zero kb file
@@ -96,7 +115,7 @@ def setUp_test_episode_file():
     if not os.path.exists(FILEDIR):
         os.makedirs(FILEDIR)
 
-    f = open(FILEPATH,"w")
+    f = open(FILEPATH, "w")
     f.write("foo bar")
     f.close()
 
@@ -108,12 +127,12 @@ if __name__ == '__main__':
     print "Dont call this directly"
     print "=================="
     print "you might want to call"
-    
-    dirList=os.listdir(TESTDIR)
+
+    dirList = os.listdir(TESTDIR)
     for fname in dirList:
         if (fname.find("_test") > 0) and (fname.find("pyc") < 0):
-            print "- "+fname
-    
+            print "- " + fname
+
     print "=================="
     print "or just call all_tests.py"
 
