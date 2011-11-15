0b4092c56eb46c6d391e5772eee0de1faea9fd8d
Josef Wells <josef.wells@alumni.utexas.net>
2011-11-15 11:56:59 -0600

    1080i is HD

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -128,7 +128,7 @@ class Quality:
             return Quality.SDTV
         elif checkName(["dvdrip.xvid", "bdrip.xvid", "dvdrip.divx", "dvdrip.ws.xvid"], any) and not checkName(["720p"], all):
             return Quality.SDDVD
-        elif checkName(["720p", "hdtv", "x264"], all) or checkName(["hr.ws.pdtv.x264"], any):
+        elif checkName(["720p", "hdtv", "x264"], all) or checkName(["1080i", "hr.ws.pdtv.x264"], any):
             return Quality.HDTV
         elif checkName(["720p", "web.dl"], all) or checkName(["720p", "itunes", "h.?264"], all):
             return Quality.HDWEBDL
