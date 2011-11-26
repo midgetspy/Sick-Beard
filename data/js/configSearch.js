c993f0df297a807196fc0b5211a7b131e43fd77b
Dennis Lutter <lad1337@gmail.com>
2011-11-26 03:20:41 +0100

    added: SABnzbd connection test button on search config page

--- data/js/configSearch.js
+++ data/js/configSearch.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+    var loading = '<img src="'+sbRoot+'/images/loading16.gif" height="16" width="16" />';
+    
 	function toggle_torrent_title(){
 		if ($('#use_torrents').prop('checked'))
 			$('#no-torrents').show();
@@ -14,14 +15,20 @@ $(document).ready(function(){
         if (selectedProvider == "blackhole") {
             $('#blackhole_settings').show();
             $('#sabnzbd_settings').hide();
+            $('#testSABnzbd').hide();
+            $('#testSABnzbd-result').hide();
             $('#nzbget_settings').hide();
         } else if (selectedProvider == "nzbget") {
             $('#blackhole_settings').hide();
             $('#sabnzbd_settings').hide();
+            $('#testSABnzbd').hide();
+            $('#testSABnzbd-result').hide();
             $('#nzbget_settings').show();
         } else {
             $('#blackhole_settings').hide();
             $('#sabnzbd_settings').show();
+            $('#testSABnzbd').show();
+            $('#testSABnzbd-result').show();
             $('#nzbget_settings').hide();
         }
 
@@ -31,6 +38,17 @@ $(document).ready(function(){
 
     $(this).nzb_method_handler();
 
+    $('#testSABnzbd').click(function(){
+        $('#testSABnzbd-result').html(loading);
+        var sab_host = $("input=[name='sab_host']").val();
+        var sab_username = $("input=[name='sab_username']").val();
+        var sab_password = $("input=[name='sab_password']").val();
+        var sab_apiKey = $("input=[name='sab_apikey']").val();
+        
+        $.get(sbRoot+"/home/testSABnzbd", {'host': sab_host, 'username': sab_username, 'password': sab_password, 'apikey': sab_apiKey}, 
+        function (data){ $('#testSABnzbd-result').html(data); });
+    });
+    
     $('#use_torrents').click(function(){
     	toggle_torrent_title();
     });
