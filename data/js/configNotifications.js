dc05199d1904e46931fb5955df5db04a2066e288
Nic Wolfe <nic@wolfeden.ca>
2012-02-23 19:34:42 -0700

    Pulled and fixed some of the XMPP code

--- data/js/configNotifications.js
+++ data/js/configNotifications.js
@@ -135,4 +135,16 @@ $(document).ready(function(){
         var nma_result = $.get(sbRoot+"/home/testNMA", {'nma_api': nma_api, 'nma_priority': nma_priority}, 
         function (data){ $('#testNMA-result').html(data); });
     });
+
+    $('#testXMPP').click(function(){
+        $('#testXMPP-result').html(loading);
+        var xmpp_username = $("#xmpp_username").val();
+        var xmpp_password = $("#xmpp_password").val();
+        var xmpp_server = $("#xmpp_server").val();
+        var xmpp_port = $("#xmpp_port").val();
+        var xmpp_recipient = $("#xmpp_recipient").val();
+        
+        $.get(sbRoot+"/home/testXMPP", {'username': xmpp_username, 'password': xmpp_password, 'server': xmpp_server, 'port': xmpp_port, 'recipient': xmpp_recipient}, 
+        function (data){ $('#testXMPP-result').html(data); });
+    });
 });
