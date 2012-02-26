3a239159478d43f4d6be745de14d7031b8048dac
Nic Wolfe <nic@wolfeden.ca>
2012-02-26 12:46:33 -0700

    Added text to explain when the config is invalid or will force folders.

--- data/js/configPostProcessing.js
+++ data/js/configPostProcessing.js
@@ -27,11 +27,25 @@ $(document).ready(function(){
         $.get(sbRoot+'/config/postProcessing/isNamingValid', {pattern: pattern, multi: multi},
             function(data){
                 if (data == "invalid")
+                {
                     $('#naming_pattern').css('background-color', '#FFDDDD');
+                    $('#custom_naming_warning_text').css('background-color', '#FFDDDD');
+                    $('#custom_naming_warning_text').html('This pattern is invalid.')
+                    $('#custom_naming_warning').show();
+                }
                 else if (data == "seasonfolders")
-                    $('#naming_pattern').css('background-color', '#FFFFDD');
+                {
+                	$('#naming_pattern').css('background-color', '#FFFFDD');
+                    $('#custom_naming_warning_text').css('background-color', '#FFFFDD');
+                    $('#custom_naming_warning_text').html('This pattern would be invalid without the folders, using it will force "Flatten" off for all shows.')
+                    $('#custom_naming_warning').show();
+                }
                 else
-                    $('#naming_pattern').css('background-color', '#FFFFFF');
+                {
+                	$('#naming_pattern').css('background-color', '#FFFFFF');
+                    $('#custom_naming_warning_text').css('background-color', '#FFFFFF');
+                    $('#custom_naming_warning').hide();
+                }
         });
 
     }
