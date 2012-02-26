ae9ce4de81163acd5d3ed5e16a16e44f1e2bdbd9
Nic Wolfe <nic@wolfeden.ca>
2012-02-26 12:51:29 -0700

    Fixed some warnings

--- sickbeard/providers/btn.py
+++ sickbeard/providers/btn.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>. 
 
-import xml.etree.cElementTree as etree
-
 import sickbeard
 import generic
 
@@ -61,8 +59,7 @@ class BTNCache(tvcache.TVCache):
 
     def _parseItem(self, item):
 
-        title = item.findtext('title')
-        url = item.findtext('link')
+        (title, url) = self.provider._get_title_and_url(item)
 
         if not title or not url:
             logger.log(u"The XML returned from the BTN RSS feed is incomplete, this result is unusable", logger.ERROR)
