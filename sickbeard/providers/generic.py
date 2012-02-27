9af67d9035cb5f54d4c6221e16d0d1668d40bd98
Harm van Tilborg <hvt@vps.zeroxcool.net>
2012-02-27 19:01:15 +0100

    Added DailyTvTorrents support

--- sickbeard/providers/generic.py
+++ sickbeard/providers/generic.py
@@ -191,7 +191,7 @@ class GenericProvider:
         quality = Quality.nameQuality(title)
         return quality
 
-    def _doSearch(self):
+    def _doSearch(self, show=None):
         return []
 
     def _get_season_search_strings(self, show, season, episode=None):
@@ -281,7 +281,7 @@ class GenericProvider:
         results = {}
 
         for curString in self._get_season_search_strings(show, season):
-            itemList += self._doSearch(curString)
+            itemList += self._doSearch(curString, show=show)
 
         for item in itemList:
 
