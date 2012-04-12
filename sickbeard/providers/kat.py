4232312b4dbde6655ae4d0a0956b13426ed2a777
Cameron Currie <me@cameroncurrie.net>
2012-04-12 15:44:18 -0500

    Handle zero result search in KAT provider

--- sickbeard/providers/kat.py
+++ sickbeard/providers/kat.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
 
-import urllib
+import urllib2
 import re
 
 import xml.etree.cElementTree as etree
@@ -27,6 +27,7 @@ import rssbacklogtorrent
 from sickbeard.common import Quality
 from sickbeard import logger
 from sickbeard import tvcache
+from sickbeard import helpers
 from sickbeard.helpers import sanitizeSceneName
 from sickbeard.exceptions import ex
 
@@ -43,7 +44,7 @@ class KATProvider(rssbacklogtorrent.RSSBacklogProvider):
         return 'kat.gif'
        
     def _get_search_url(self, show_snippet):
-        return self.url + 'search/' + urllib.quote(show_snippet.strip()) + '/?rss=1'
+        return self.url + 'search/' + urllib2.quote(show_snippet.strip()) + '/?rss=1'
        
     def _get_title_and_url(self, item):
         return self._get_torrent_title_and_url(item)
@@ -64,6 +65,26 @@ class KATProvider(rssbacklogtorrent.RSSBacklogProvider):
         seeds = item.findtext('seeds')
         logger.log(rawTitle + " had " + seeds + " seeds", logger.DEBUG)
         return int(seeds) >= sickbeard.KAT_MINIMUM_SEEDS # Minimum number of seeds
+
+    def getURL(self, url, headers=None):
+        """
+        Overridden because KAT's site yields an HTTPError if zero results are returned
+        """
+
+        if not headers:
+            headers = []
+
+        result = None
+
+        try:
+            result = helpers.getURL(url, headers)
+        except urllib2.HTTPError as e:
+            logger.log(u"KAT returned HTTPError, interpreting as zero result search " + ex(e), logger.DEBUG)
+            logger.log(u"No results found on KickassTorrents")
+        except IOError as e:
+            logger.log(u"Error loading "+self.name+" URL: " + str(sys.exc_info()) + " - " + ex(e))
+
+        return result
 
 class KATRSSCache(rssbacklogtorrent.RSSCache):
     def __init__(self, provider):
