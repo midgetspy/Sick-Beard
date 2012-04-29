489957753dcf87db032438b4ef3b499520a8518a
Nic Wolfe <nic@wolfeden.ca>
2012-04-29 13:27:20 -0600

    Fix some small issues with the DailyTvTorrents provider

--- sickbeard/providers/dtt.py
+++ sickbeard/providers/dtt.py
@@ -17,7 +17,6 @@
 # along with Sick Beard.  If not, see <http://www.gnu.org/licenses/>.
 
 import urllib
-import re
 
 from xml.dom.minidom import parseString
 
@@ -36,7 +35,7 @@ class DTTProvider(generic.TorrentProvider):
         generic.TorrentProvider.__init__(self, "DailyTvTorrents")
         self.supportsBacklog = True
         self.cache = DTTCache(self)
-        self.url = 'http://www.dailytvtorrents.org/rss/'
+        self.url = 'http://www.dailytvtorrents.org/'
 
     def isEnabled(self):
         return sickbeard.DTT
@@ -72,12 +71,12 @@ class DTTProvider(generic.TorrentProvider):
     def _doSearch(self, params, show=None):
         show_id = self._dtt_show_id(show.name)
 
-        search_params = {"single": "yes"}
+        search_params = {"norar": "yes"}
 
         if params:
             search_params.update(params)
 
-        searchURL = self.url + "show/" + show_id + "?" + urllib.urlencode(search_params)
+        searchURL = self.url + "rss/show/" + show_id + "?" + urllib.urlencode(search_params)
 
         logger.log(u"Search string: " + searchURL, logger.DEBUG)
 
@@ -120,7 +119,7 @@ class DTTCache(tvcache.TVCache):
 
 
     def _getRSSData(self):
-        url = self.provider.url + 'allshows?single=yes'
+        url = self.provider.url + 'rss/allshows?norar=yes'
         logger.log(u"DTT cache update URL: "+ url, logger.DEBUG)
         data = self.provider.getURL(url)
         return data
