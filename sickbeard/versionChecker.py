146629b14ade8693d8dfeb0fd454277305d10b9f
 Luca <l.dimarino@gmail.com>
2012-10-27 20:37:57 +0300

    Fix VersionChecker not retriving newest commits

--- sickbeard/versionChecker.py
+++ sickbeard/versionChecker.py
@@ -311,8 +311,7 @@ class GitUpdateManager(UpdateManager):
         gh = github.GitHub()
 
         # find newest commit
-        for curCommit in gh.commits('mr-orange', 'Sick-Beard', self.branch):
-#        for curCommit in gh.commits('mr-orange', 'Sick-Beard', version.SICKBEARD_VERSION):
+        for curCommit in gh.commits('mr-orange', 'Sick-Beard', version.SICKBEARD_VERSION):
 
             if not self._newest_commit_hash:
                 self._newest_commit_hash = curCommit['sha']
