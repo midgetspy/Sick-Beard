c1ff324316e8fc0baba84e150b076224321ebd86
cytec <iamcytec@googlemail.com>
2012-11-07 12:59:51 +0100

    sqlalchemy added to lib

---
+++ lib/sqlalchemy/dialects/__init__.py
@@ -0,0 +1,19 @@
+# dialects/__init__.py
+# Copyright (C) 2005-2012 the SQLAlchemy authors and contributors <see AUTHORS file>
+#
+# This module is part of SQLAlchemy and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+__all__ = (
+#    'access',
+    'drizzle',
+    'firebird',
+#    'informix',
+#    'maxdb',
+    'mssql',
+    'mysql',
+    'oracle',
+    'postgresql',
+    'sqlite',
+    'sybase',
+    )
