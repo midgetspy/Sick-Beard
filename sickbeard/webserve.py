d6199ed2d48914c618d9d16f5513bd8f5c19f1b7
Luca <l.dimarino@gmail.com>
2012-11-10 20:50:15 +0100

    FIX The fix of last commit

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -3039,7 +3039,7 @@ class Home:
             # Only want to rename if we have a location
             if cur_ep_obj.location:
                 if cur_ep_obj.relatedEps:
-                # do we have one of multi-episodes in the rename list already
+                    # do we have one of multi-episodes in the rename list already
                     have_already = False
                     for cur_related_ep in cur_ep_obj.relatedEps + [cur_ep_obj]:
                         if cur_related_ep in ep_obj_rename_list:
@@ -3047,12 +3047,12 @@ class Home:
                             break
                         if not have_already:
                             ep_obj_rename_list.append(cur_ep_obj)
-            else:
-                ep_obj_rename_list.append(cur_ep_obj)
+                else:
+                    ep_obj_rename_list.append(cur_ep_obj)
 
-            if ep_obj_rename_list:
-                # present season DESC episode DESC on screen
-                ep_obj_rename_list.reverse()
+        if ep_obj_rename_list:
+            # present season DESC episode DESC on screen
+            ep_obj_rename_list.reverse()
 
         t = PageTemplate(file="testRename.tmpl")
         t.submenu = [{'title': 'Edit', 'path': 'home/editShow?show=%d' % showObj.tvdbid}]
