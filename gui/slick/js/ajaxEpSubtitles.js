001177ada1b8ac4c9f964e451d3420a6d59019dd
Luca <l.dimarino@gmail.com>
2012-11-20 20:05:39 +0100

    Css Subtitles Styling on Manual Search

--- gui/slick/js/ajaxEpSubtitles.js
+++ gui/slick/js/ajaxEpSubtitles.js
@@ -1,4 +1,6 @@
 (function(){
+		$.ajaxSetup({ async: false });
+
 	$.fn.ajaxEpSubtitlesSearch = function(){
 		$('.epSubtitlesSearch').click(function(){
 			var subtitles_td = $(this).parent().siblings('.subtitles_column');
@@ -7,18 +9,25 @@
 			subtitles_search_link.empty();
 			subtitles_search_link.append($("<img/>").attr({"src": sbRoot+"/images/loading16_dddddd.gif", "alt": "", "title": "loading"}));
 			$.getJSON($(this).attr('href'), function(data){
+				if (data.result != "failure" && data.result != "No subtitles downloaded") {
 				// update the subtitles column with new informations
 				var subtitles = data.subtitles.split(',');
-				subtitles_td.html("&nbsp;");
 				$.each(subtitles,function(index, language){
 					if (language != "") {
-						subtitles_td.append($("<img/>").attr({"src": sbRoot+"/images/flags/"+language+".png", "alt": language, "width": 16, "height": 11}));
-						subtitles_td.append("&nbsp;");
+						if (index != subtitles.length - 1) {
+							subtitles_td.append($("<img/>").attr({"src": sbRoot+"/images/flags/"+language+".png", "alt": language, "width": 16, "height": 11}).css({'padding-right' : '6px'}));	
+					  } else {
+					  	subtitles_td.append($("<img/>").attr({"src": sbRoot+"/images/flags/"+language+".png", "alt": language, "width": 16, "height": 11}));
+					  }
 					}
-				});
+				});			
 				// don't allow other searches
 				subtitles_search_link.remove();
+				} else {
+					subtitles_search_link.remove();
+				}
 			});
+			
 			// don't follow the link
 			return false;
 		});
