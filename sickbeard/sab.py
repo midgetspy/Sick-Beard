548f5184a4cb887b397915e30f2e84d01f50d50c
Raphael <info@raphaelmutschler.de>
2012-11-20 17:56:09 +0100

    fix nzbinde prettyname

--- sickbeard/sab.py
+++ sickbeard/sab.py
@@ -76,6 +76,10 @@ def sendNZB(nzb):
         params['mode'] = 'addfile'
         multiPartParams = {"nzbfile": (nzb.name + ".nzb", nzb.extraInfo[0])}
 
+    if nzb.resultType == "nzb" and nzb.provider.getID() == 'nzbindex':
+        logger.log(u"Pretty name for SAB queue: " + nzb.name)
+        params['nzbname'] = nzb.name
+
     url = sickbeard.SAB_HOST + "api?" + urllib.urlencode(params)
 
     logger.log(u"Sending NZB to SABnzbd")
@@ -84,9 +88,7 @@ def sendNZB(nzb):
     try:
         #Sometimes (only from nzbindex results?) we have stupid names in sab queue - postprocessing does not work then.
         #Should use &nzbname=NiceName here to send the "real" name to sab.
-        if nzb.resultType == "nzb" and nzb.provider.getID() == 'nzbindex':
-            logger.log(u"Pretty name for SAB queue: " + nzb.name)
-            params['nzbname'] = nzb.name
+        
 
         # if we have the URL to an NZB then we've built up the SAB API URL already so just call it 
         if nzb.resultType == "nzb":
