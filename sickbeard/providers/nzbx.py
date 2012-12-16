3b9f7e570a61687f16f46a82f11bdb2a113fef29
Ian Quick <ian.quick@gmail.com>
2012-12-16 16:33:02 -0500

    removed debug line that was crashing

--- sickbeard/providers/nzbx.py
+++ sickbeard/providers/nzbx.py
@@ -156,7 +156,7 @@ class NzbxProvider(generic.NZBProvider):
         else:
             params['q'] = helpers.sanitizeSceneName(ep_obj.show.name)
         
-        logger.log(params['q'] )
+            #logger.log(params['q'] )
 
 
 
