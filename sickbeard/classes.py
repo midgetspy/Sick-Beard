885f1c44b672b20627016f69e6dbe6324b4b4cff
Nic Wolfe <nic@wolfeden.ca>
2013-02-02 22:00:48 -0700

    Fixed some bugs with episodes ending up in an unknown state and causing errors

--- sickbeard/classes.py
+++ sickbeard/classes.py
@@ -23,7 +23,7 @@ import sickbeard
 import urllib
 import datetime
 
-from common import USER_AGENT
+from common import USER_AGENT, Quality
 
 class SickBeardURLopener(urllib.FancyURLopener):
     version = USER_AGENT
@@ -84,7 +84,7 @@ class SearchResult:
         self.episodes = episodes
 
         # quality of the release
-        self.quality = -1
+        self.quality = Quality.UNKNOWN
 
         # release name
         self.name = ""
@@ -149,7 +149,7 @@ class Proper:
         self.url = url
         self.date = date
         self.provider = None
-        self.quality = -1
+        self.quality = Quality.UNKNOWN
 
         self.tvdbid = -1
         self.season = -1
