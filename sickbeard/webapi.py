d9da4857cbea2e2eba05895ef00bbf274aff6d93
Bo Osterud <git@conjuro.net>
2013-02-27 10:21:38 +0100

    Enhancement :: Show listings : Enable choice of sorting show lists by articles (the, a/an).

--- sickbeard/webapi.py
+++ sickbeard/webapi.py
@@ -126,10 +126,12 @@ class Api:
         t = webserve.PageTemplate(file="apiBuilder.tmpl")
 
         def titler(x):
-            if not x:
+            if not x or sickbeard.SORT_ARTICLE:
                 return x
             if x.lower().startswith('a '):
                 x = x[2:]
+            elif x.lower().startswith('an '):
+                x = x[3:]
             elif x.lower().startswith('the '):
                 x = x[4:]
             return x
