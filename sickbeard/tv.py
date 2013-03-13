83e02576b27d8a8fe47e0798f71cecedc8c9fe06
Luca <l.dimarino@gmail.com>
2013-03-13 12:04:24 +0100

    Subtitle language notification FIX (Isetz)

--- sickbeard/tv.py
+++ sickbeard/tv.py
@@ -1173,17 +1173,17 @@ class TVEpisode(object):
             return
 
         self.refreshSubtitles()
-        self.subtitles_searchcount = self.subtitles_searchcount + 1
+        self.subtitles_searchcount = self.subtitles_searchcount + 1 if self.subtitles_searchcount else 1 #added the if because sometime it raise an error
         self.subtitles_lastsearch = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.saveToDB()
         
         newsubtitles = set(self.subtitles).difference(set(previous_subtitles))
         
         if newsubtitles:
-            subtitleList = (subliminal.language.Language(x).name for x in newsubtitles)
-            logger.log(str(self.show.tvdbid) + ": Downloaded " + ", ".join(subtitleList) + " subtitles for episode " + str(self.season) + "x" + str(self.episode), logger.DEBUG)
+            subtitleList = ", ".join(subliminal.language.Language(x).name for x in newsubtitles)
+            logger.log(str(self.show.tvdbid) + ": Downloaded " + subtitleList + " subtitles for episode " + str(self.season) + "x" + str(self.episode), logger.DEBUG) 
             
-            notifiers.notify_subtitle_download(self.prettyName(), ", ".join(subtitleList))
+            notifiers.notify_subtitle_download(self.prettyName(), subtitleList)
 
         else:
             logger.log(str(self.show.tvdbid) + ": No subtitles downloaded for episode " + str(self.season) + "x" + str(self.episode), logger.DEBUG)
