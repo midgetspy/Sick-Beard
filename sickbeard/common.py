e97a0261254d5bcb38ac92ed2d088b6e8b62d49f
cytec <iamcytec@googlemail.com>
2013-03-22 17:51:32 +0100

    add itunesHD avc to 720p WEB-DL

--- sickbeard/common.py
+++ sickbeard/common.py
@@ -155,7 +155,7 @@ class Quality:
             return Quality.RAWHDTV
         elif checkName(["1080p", "hdtv", "x264"], all):
             return Quality.FULLHDTV
-        elif checkName(["720p", "web.dl|webrip"], all) or checkName(["720p", "(webhd|itunes)", "h.?264"], all):
+        elif checkName(["720p", "web.dl|webrip"], all) or checkName(["720p", "(webhd|itunes)", "h.?264"], all) or checkName(["720p", "(webhd|itunes)", "avc"], all):
             return Quality.HDWEBDL
         elif checkName(["1080p", "web.dl|webrip"], all) or checkName(["1080p", "itunes", "h.?264"], all):
             return Quality.FULLHDWEBDL
