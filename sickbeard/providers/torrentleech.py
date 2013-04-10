a9ee3812f9942997b0bada4ad13471eceb2aeb8d
Maor Tal <maortl@gmail.com>
2013-04-11 02:00:20 +0300

    bug fix to torrentleech manual search
    function returned empty list even when results were found earlier
    
    should be break and not return []
    
    now it works good

--- sickbeard/providers/torrentleech.py
+++ sickbeard/providers/torrentleech.py
@@ -153,7 +153,7 @@ class TorrentLeechProvider(generic.TorrentProvider):
         
                 data = self.getURL(searchURL)
                 if not data:
-                    return []
+                    break
 
                 try:
                     html = BeautifulSoup(data)
@@ -162,7 +162,7 @@ class TorrentLeechProvider(generic.TorrentProvider):
                     
                     if not torrent_table:
                         logger.log(u"No results found for: " + search_string + "(" + searchURL + ")", logger.DEBUG)
-                        return []
+                        break
 
                     for result in torrent_table.find_all('tr')[1:]:
 
