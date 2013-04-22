306fc499201125cad27d3b7f3c048903aec1d71f
Pedro José Pereira Vieito <pvieito@gmail.com>
2013-04-22 20:00:22 +0200

    iCalendar Support
    iCalendar with episodes one year before and after the
    present day. You can subscribe from Coming Episodes page or “/calendar”.
    
    Works with iCloud, Google Calendar and Outlook.

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -3650,6 +3650,58 @@ class WebInterface:
                 
 
         return _munge(t)
+        
+    # Raw iCalendar implementation by Pedro Jose Pereira Vieito (@pvieito).
+    #
+    # iCalendar (iCal) - Standard RFC 5545 <http://tools.ietf.org/html/rfc5546> 
+    # Works with iCloud, Google Calendar and Outlook.
+    @cherrypy.expose
+    def calendar(self):
+        """ Provides a subscribeable URL for iCal subscriptions
+        """
+
+        logger.log(u"Receiving iCal request from %s" % cherrypy.request.remote.ip)
+
+        poster_url = cherrypy.url().replace('ical', '')
+
+        time_re = re.compile('([0-9]{1,2})\:([0-9]{2})(\ |)([AM|am|PM|pm]{2})')
+
+		# Create a iCal string        
+        ical = 'BEGIN:VCALENDAR\n' 
+        ical += 'VERSION:2.0\n'
+        ical += 'PRODID://Sick-Beard Upcoming Episodes//\n'
+
+        # Get shows info
+        myDB = db.DBConnection()
+        
+        # Limit dates
+        past_date = (datetime.date.today() + datetime.timedelta(weeks=-52)).toordinal()
+        future_date = (datetime.date.today() + datetime.timedelta(weeks=52)).toordinal()
+        
+        # Get all the shows that are not paused and are currently on air (from kjoconnor Fork)
+        calendar_shows = myDB.select("SELECT show_name, tvdb_id, network, airs, runtime FROM tv_shows WHERE status = 'Continuing' AND paused != '1'")
+        for show in calendar_shows:
+            # Get all episodes of this show airing between today and next month
+            episode_list = myDB.select("SELECT tvdbid, name, season, episode, description, airdate FROM tv_episodes WHERE airdate >= ? AND airdate < ? AND showid = ?", (past_date, future_date, int(show["tvdb_id"])))
+
+            for episode in episode_list:
+                
+                # SickBeard usually downloads episodes one day later
+                air_date = datetime.datetime.fromordinal(int(episode['airdate'])).date() + datetime.timedelta(days=1)
+                
+                # Create event for episode
+                ical = ical + 'BEGIN:VEVENT\n'
+                ical = ical + 'DTSTART:' + str(air_date).replace("-", "") + '\n'
+                ical = ical + 'SUMMARY:' + show['show_name'] + ': ' + episode['name'] + '\n'                
+                ical = ical + 'UID:' + str(datetime.date.today().isoformat()) + '-' + str(random.randint(10000,99999)) + '@Sick-Beard\n'
+                ical = ical + 'DESCRIPTION:' + episode['description'] + '\n'
+                ical = ical + 'LOCATION:' + 'Episode ' + str(episode['episode']) + ' - Season ' + str(episode['season']) + '\n'
+                ical = ical + 'END:VEVENT\n'
+
+        # Ending the iCal
+        ical += 'END:VCALENDAR\n' 
+        
+        return ical
 
     manage = Manage()
 
