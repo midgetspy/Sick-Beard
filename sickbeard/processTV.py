8d8e0e3318258d561b09d674d2b8db606202e995
Luca <l.dimarino@gmail.com>
2013-04-23 12:31:21 +0200

    PostProcessor Fixes

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -42,6 +42,20 @@ def processDir (dirName, nzbName=None, recurse=False):
     nzbName: The NZB name which resulted in this folder being downloaded
     """
 
+    REMOTE_DBG = True
+    
+    if REMOTE_DBG:
+            # Make pydev debugger works for auto reload.
+            # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
+        try:
+            import pysrc.pydevd as pydevd
+            # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
+            pydevd.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
+        except ImportError:
+            sys.stderr.write("Error: " +
+                    "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
+            sys.exit(1) 
+
     returnStr = ''
 
     returnStr += logHelper(u"Processing folder "+dirName, logger.DEBUG)
@@ -104,7 +118,7 @@ def processDir (dirName, nzbName=None, recurse=False):
     #Process Video File in all TV Subdir
     for dir in [x for x in dirs if validateDir(path, x, returnStr)]:
 
-        process_result = False
+#        process_result = False
 
         for processPath, processDir, fileList in ek.ek(os.walk, ek.ek(os.path.join, path, dir), topdown=False):
 
