c1a85b49dcf6956f5f181b864195c3eb1e6fc266
Sarakha63 <sarakha_ludovic@yahoo.fr>
2013-05-18 23:52:43 +0200

    Correction of search settings screen

--- data/js/configSearch.js
+++ data/js/configSearch.js
@@ -115,13 +115,10 @@ $(document).ready(function(){
     $('#use_torrents').click(function(){
     	toggle_torrent_title();
     	
+    });
     $('#prefered_method').change($(this).prefered_method_handler);
 	
 	$(this).prefered_method_handler();
-    
-    $('#use_torrents').click(function(){
-    	toggle_torrent_title();
-    });
 
     $('#testTorrent').click(function(){
         $('#testTorrent-result').html(loading);
