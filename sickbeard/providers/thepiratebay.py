94f6a990d201fc3a099c5eb5bc01e5e53d04797b
Luca <l.dimarino@gmail.com>
2013-07-26 12:16:31 +0200

    Removing Debugging routine

--- sickbeard/providers/thepiratebay.py
+++ sickbeard/providers/thepiratebay.py
@@ -213,21 +213,6 @@ class ThePirateBayProvider(generic.TorrentProvider):
 
     def _doSearch(self, search_params, show=None):
 
-        REMOTE_DBG = True
-        
-        if REMOTE_DBG:
-                # Make pydev debugger works for auto reload.
-                # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
-            try:
-                import pysrc.pydevd as pydevd
-                # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
-                pydevd.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
-            except ImportError:
-                sys.stderr.write("Error: " +
-                        "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
-                sys.exit(1) 
-
-    
         results = []
         items = {'Season': [], 'Episode': []}
 
