d8cb3bf74b6945f6bca822cea8c1ab7c73d61c95
Raphael Mutschler <info@raphaelmutschler.de>
2013-08-23 12:12:19 +0200

    fixed typo

--- sickbeard/tvcache.py
+++ sickbeard/tvcache.py
@@ -358,7 +358,7 @@ class TVCache():
         for curResult in sqlResults:
 
             # skip non-tv crap (but allow them for Newzbin cause we assume it's filtered well)
-            if self.providerID != 'newzbin' and not show_name_helpers.filterBadReleases(curResult["name"]):
+            if self.providerID != 'newzbin' and not show_name_helpers.filterBadReleases(curResult["name"], episode.show):
                 continue
 
             # get the show object, or if it's not one of our shows then ignore it
