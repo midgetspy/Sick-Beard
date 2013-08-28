0d973f51fbe410c8ad7880e93c0b84071ee11d1d
Luca <l.dimarino@gmail.com>
2013-08-28 16:38:52 +0200

    Damn Debugger.

--- sickbeard/processTV.py
+++ sickbeard/processTV.py
@@ -123,20 +123,6 @@ def processDir (dirName, nzbName=None, recurse=False):
         else:
             returnStr += logHelper(u"Processing failed for "+cur_video_file_path+": "+process_fail_message, logger.WARNING)
 
-    REMOTE_DBG = True
-    
-    if REMOTE_DBG:
-            # Make pydev debugger works for auto reload.
-            # Note pydevd module need to be copied in XBMC\system\python\Lib\pysrc
-        try:
-            import pysrc.pydevd as pydevd
-            # stdoutToServer and stderrToServer redirect stdout and stderr to eclipse console
-            pydevd.settrace('localhost', port=5678, stdoutToServer=True, stderrToServer=True)
-        except ImportError:
-            sys.stderr.write("Error: " +
-                    "You must add org.python.pydev.debug.pysrc to your PYTHONPATH.")
-            sys.exit(1) 
-
     #Process Video File in all TV Subdir
     for dir in [x for x in dirs if validateDir(path, x)]:
         
