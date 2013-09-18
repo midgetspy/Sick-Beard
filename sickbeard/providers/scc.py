cebdc1fec53e009b7dfee62dbfcce8e5e48f9e47
Mr_Orange <l.dimarino@gmail.com>
2013-09-18 11:55:25 +0200

    SCC Provider: Don't verify SSL on login either

--- sickbeard/providers/scc.py
+++ sickbeard/providers/scc.py
@@ -77,7 +77,7 @@ class SCCProvider(generic.TorrentProvider):
         self.session = requests.Session()
 
         try:
-            response = self.session.post(self.urls['login'], data=login_params, timeout=30)
+            response = self.session.post(self.urls['login'], data=login_params, timeout=30, verify=False)
         except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError), e:
             logger.log(u'Unable to connect to ' + self.name + ' provider: ' +ex(e), logger.ERROR)
             return False
