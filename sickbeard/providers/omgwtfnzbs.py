3d65aa236a39cc2801643ccf7ff17f0d066e31db
Patrick Vos <itofzo@gmail.com>
2013-10-24 15:05:11 +0200

    Change switch omgwtfnzbs to https

--- sickbeard/providers/omgwtfnzbs.py
+++ sickbeard/providers/omgwtfnzbs.py
@@ -44,7 +44,7 @@ class OmgwtfnzbsProvider(generic.NZBProvider):
     def __init__(self):
         generic.NZBProvider.__init__(self, "omgwtfnzbs")
         self.cache = OmgwtfnzbsCache(self)
-        self.url = 'http://omgwtfnzbs.org/'
+        self.url = 'https://omgwtfnzbs.org/'
         self.supportsBacklog = True
 
     def isEnabled(self):
@@ -167,7 +167,7 @@ class OmgwtfnzbsCache(tvcache.TVCache):
                   'eng': 1,
                   'catid': '19,20'}  # SD,HD
 
-        rss_url = 'http://rss.omgwtfnzbs.org/rss-download.php?' + urllib.urlencode(params)
+        rss_url = 'https://rss.omgwtfnzbs.org/rss-download.php?' + urllib.urlencode(params)
 
         logger.log(self.provider.name + u" cache update URL: " + rss_url, logger.DEBUG)
 
