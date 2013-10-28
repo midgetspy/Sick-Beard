cc8363cfa27cee8e721810fa10974d5465d12ee3
nivong <nvoorn@gmail.com>
2013-10-28 19:57:35 +0100

    Update versionChecker.py

--- sickbeard/versionChecker.py
+++ sickbeard/versionChecker.py
@@ -113,7 +113,7 @@ class CheckVersion():
 class UpdateManager():
 
     def get_github_repo_user(self):
-        return 'mr-orange'
+        return 'nivong'
 
     def get_github_repo(self):
         return 'Sick-Beard'
