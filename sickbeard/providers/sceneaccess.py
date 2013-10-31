aa5fee1f33841cdc56257ec10bc4033c7513b300
VeNoMouS <venom@gen-x.co.nz>
2013-10-31 20:45:27 +1300

    Sceneaccess cache helpers.getURL replace

--- sickbeard/providers/sceneaccess.py
+++ sickbeard/providers/sceneaccess.py
@@ -218,7 +218,7 @@ class SceneAccessCache(tvcache.TVCache):
         if sickbeard.SCENEACCESS_RSSHASH:
             self.rss_url = "https://sceneaccess.eu/rss?feed=dl&cat=27,17,11&passkey={0}".format(sickbeard.SCENEACCESS_RSSHASH)
             logger.log("[" + provider.name + "] RSS URL - {0}".format(self.rss_url))
-            xml = helpers.getURL(self.rss_url)
+            xml = provider.getURL(self.rss_url)
         else:
             logger.log("[" + provider.name + "] WARNING: RSS construction via browse since no hash provided.")
             url = provider.url + "browse?search=&method=2&c27=27&c17=17&c11=11"
