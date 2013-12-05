604ac1a0083121933209112a72acde24a42d7ff1
Stan Kardach <kardasan@gmail.com>
2013-12-05 20:22:24 +0100

    Correct checking for TV Download Dir deletion
    At the beginning of processTV.processDir, the path given in
    dirName is expanded with os.path.realpath, which eliminates all
    symbolic links. Later, when script checks whether current
    post-processed folder should be deleted, it tries to match the
    dirName against the sickbeard.TV_DOWNLOAD_DIR. However the
    during that check, the second path is not expanded with
    os.path.realpath which results in an unmatched condition and
    hence deletion of the TV Download Dir if the path given in the
    configuration is a symbolic link.

--- tests/pp_tests.py
+++ tests/pp_tests.py
@@ -22,12 +22,25 @@ import unittest
 
 import test_lib as test
 
-import sys, os.path
+import sys, os.path, os, shutil
 
 from sickbeard.postProcessor import PostProcessor
+
 import sickbeard
 from sickbeard.tv import TVEpisode, TVShow
+from sickbeard.processTV import processDir
 
+def init_shows():
+    show = TVShow(3)
+    show.name = test.SHOWNAME
+    show.location = test.SHOWDIR
+    show.saveToDB()
+
+    sickbeard.showList = [show]
+    ep = TVEpisode(show, test.SEASON, test.EPISODE)
+    ep.name = "some ep name"
+    ep.saveToDB()
+    return (show, ep)
 
 class PPInitTests(unittest.TestCase):
 
@@ -78,19 +91,34 @@ class PPPrivateTests(test.SickbeardTestDBCase):
         ecpectedPath = os.path.join(test.FILEDIR, "Season 0" + str(test.SEASON))
         self.assertEqual(calculatedPath, ecpectedPath)
 
+    def test_do_not_delete_tv_download_dir(self):
+        TEST_DOWNLOAD_DIR = u"tv_download_dir"
+        TEST_DOWNLOAD_DIR_LINK = u"tv_download_dir_link"
+        TEST_NEW_EP_FILE = u"show name - s01e010.mkv"
+
+        (show, ep) = init_shows()
+
+        old_download_dir = sickbeard.TV_DOWNLOAD_DIR
+        sickbeard.TV_DOWNLOAD_DIR = TEST_DOWNLOAD_DIR_LINK
+        if(os.path.exists(TEST_DOWNLOAD_DIR_LINK)):
+            os.remove(TEST_DOWNLOAD_DIR_LINK)
+        if(os.path.exists(TEST_DOWNLOAD_DIR)):
+            shutil.rmtree(TEST_DOWNLOAD_DIR)
+        os.mkdir(TEST_DOWNLOAD_DIR)
+        os.symlink(TEST_DOWNLOAD_DIR, TEST_DOWNLOAD_DIR_LINK)
+        open(os.path.join(TEST_DOWNLOAD_DIR_LINK, TEST_NEW_EP_FILE), "a").close()
+        processDir(TEST_DOWNLOAD_DIR_LINK)
+
+        sickbeard.TV_DOWNLOAD_DIR = old_download_dir
+        self.assertTrue(os.path.exists(TEST_DOWNLOAD_DIR))
+
+        shutil.rmtree(TEST_DOWNLOAD_DIR)
+        os.remove(TEST_DOWNLOAD_DIR_LINK)
 
 class PPBasicTests(test.SickbeardTestDBCase):
 
     def test_process(self):
-        show = TVShow(3)
-        show.name = test.SHOWNAME
-        show.location = test.SHOWDIR
-        show.saveToDB()
-
-        sickbeard.showList = [show]
-        ep = TVEpisode(show, test.SEASON, test.EPISODE)
-        ep.name = "some ep name"
-        ep.saveToDB()
+        (show, ep) = init_shows()
 
         pp = PostProcessor(test.FILEPATH)
         self.assertTrue(pp.process())
