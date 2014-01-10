fef948413ef73903c7288570de4b43870d302bbd
Luca <l.dimarino@gmail.com>
2014-01-10 12:38:34 +0100

    Refresh metadata div when changing dropdown with arrow keys (zoggy)

--- gui/slick/js/configPostProcessing.js
+++ gui/slick/js/configPostProcessing.js
@@ -191,7 +191,7 @@ $(document).ready(function () {
     setup_abd_naming();
 
     // -- start of metadata options div toggle code --
-    $('#metadataType').change(function () {
+    $('#metadataType').on('change keyup', function () {
         $(this).showHideMetadata();
     });
 
@@ -207,7 +207,7 @@ $(document).ready(function () {
             }
         });
     };
-    //initalize to show the div
+    //initialize to show the div
     $(this).showHideMetadata();
     // -- end of metadata options div toggle code --
 
