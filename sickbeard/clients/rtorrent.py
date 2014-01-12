e0062eb451e706a22b0ace0fa7fb665eef0b4c13
Skinder <stephan.schamp@gmail.com>
2014-01-12 07:07:05 +0100

    Rewrite of the rTorrent client (general) and library (magnet support).

--- sickbeard/clients/rtorrent.py
+++ sickbeard/clients/rtorrent.py
@@ -40,16 +40,11 @@ class rTorrentAPI(GenericClient):
         if self.username and self.password:
             self.auth = Rtorrent(self.host, self.username, self.password)
         else:
-            self.auth = RTorrent(self.host)
+            self.auth = RTorrent(self.host, None, None, True)
 
         return self.auth
 
     def _add_torrent_uri(self, result):
-        return _add_torrent_file(self, result)
-
-    def _add_torrent_file(self, result):
-        print result.name
-        print result.url
         filedata = None
 
         if not self.auth:
@@ -58,15 +53,45 @@ class rTorrentAPI(GenericClient):
         if not result:
             return False
 
-        group_name = 'sb_test'.lower() ##### Use provider instead of _test
-        if not self._set_torrent_ratio(group_name):
+        try:
+            # Send magnet to rTorrent
+            torrent = self.auth.load_magnet(result.url, result.hash)
+
+            if not torrent:
+                return False
+
+            # Set label
+            if sickbeard.TORRENT_LABEL:
+                torrent.set_custom(1, sickbeard.TORRENT_LABEL.lower())
+
+            if sickbeard.TV_DOWNLOAD_DIR:
+                torrent.set_directory(sickbeard.TV_DOWNLOAD_DIR)
+
+            # Start torrent
+            torrent.start()
+
+            return True
+
+        except:
             return False
+
+    def _add_torrent_file(self, result):
+        filedata = None
+
+        if not self.auth:
+            return False
+
+        if not result:
+            return False
+
+        # group_name = 'sb_test'.lower() ##### Use provider instead of _test
+        # if not self._set_torrent_ratio(group_name):
+            # return False
 
         # Send request to rTorrent
         try:
             # Send torrent to rTorrent
-            torrent = self.auth.load_torrent(result.name + '.torrent', result.content)
-            print torrent
+            torrent = self.auth.load_torrent(result.content)
 
             if not torrent:
                 return False
@@ -79,50 +104,61 @@ class rTorrentAPI(GenericClient):
                 torrent.set_directory(sickbeard.TV_DOWNLOAD_DIR)
 
             # Set Ratio Group
-            torrent.set_visible(group_name)
+            # torrent.set_visible(group_name)
 
             # Start torrent
             torrent.start()
 
-            return self.torrent
+            return True
 
         except:
             return False
 
     def _set_torrent_ratio(self, name):
 
-        if not name:
-            return False
+        # if not name:
+            # return False
 
-        if not self.auth:
-            return False
+        # if not self.auth:
+            # return False
 
-        views = self.auth.get_views()
+        # views = self.auth.get_views()
 
-        if name not in views:
-            self.auth.create_group(name)
+        # if name not in views:
+            # self.auth.create_group(name)
 
-        group = self.auth.get_group(name)
+        # group = self.auth.get_group(name)
 
-        ratio = int(float(sickbeard.TORRENT_RATIO) * 100)
+        # ratio = int(float(sickbeard.TORRENT_RATIO) * 100)
 
-        try:
-            if ratio > 0:
+        # try:
+            # if ratio > 0:
 
-                # Explicitly set all group options to ensure it is setup correctly
-                group.set_upload('1M')
-                group.set_min(ratio)
-                group.set_max(ratio)
-                group.set_command('d.stop')
-                group.enable()
-            else:
-                # Reset group action and disable it
-                group.set_command()
-                group.disable()
+                # # Explicitly set all group options to ensure it is setup correctly
+                # group.set_upload('1M')
+                # group.set_min(ratio)
+                # group.set_max(ratio)
+                # group.set_command('d.stop')
+                # group.enable()
+            # else:
+                # # Reset group action and disable it
+                # group.set_command()
+                # group.disable()
 
-        except:
-            return False
+        # except:
+            # return False
 
         return True
+
+    def testAuthentication(self):
+        try:
+            self._get_auth()
+
+            if self.auth is not None:
+                return True, 'Success: Connected and Authenticated'
+            else:
+                return False, 'Error: Unable to get ' + self.name + ' Authentication, check your config!'
+        except Exception:
+            return False, 'Error: Unable to connect to '+ self.name
 
 api = rTorrentAPI()
