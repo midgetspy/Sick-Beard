914cf7f5e20dec857059e4e9d313728f352ce13c
mr-orange <mr_orange@hotmail.it>
2014-03-03 15:13:51 +0100

    SCC Proper user-agent authentication header fix

--- sickbeard/providers/scc.py
+++ sickbeard/providers/scc.py
@@ -228,10 +228,10 @@ class SCCProvider(generic.TorrentProvider):
             self._doLogin()
 
         if not headers:
-            headers = []
+            headers = {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'}
 
         try:
-            response = self.session.get(url, verify=False)
+            response = self.session.get(url, headers=headers, verify=False)
         except (requests.exceptions.ConnectionError, requests.exceptions.HTTPError), e:
             logger.log(u"Error loading "+self.name+" URL: " + ex(e), logger.ERROR)
             return None
