ad3a75d06e0ce5d89116cbd14e018399821b43b7
Patrick Vos <itofzo@gmail.com>
2014-03-13 18:46:14 +0100

    Add ek.ek on os.stat parentPath

--- sickbeard/helpers.py
+++ sickbeard/helpers.py
@@ -460,7 +460,8 @@ def chmodAsParent(childPath):
         logger.log(u"No parent path provided in " + childPath + ", unable to get permissions from it", logger.DEBUG)
         return
 
-    parentMode = stat.S_IMODE(os.stat(parentPath)[stat.ST_MODE])
+    parentPathStat = ek.ek(os.stat, parentPath)
+    parentMode = stat.S_IMODE(parentPathStat[stat.ST_MODE])
 
     childPathStat = ek.ek(os.stat, childPath)
     childPath_mode = stat.S_IMODE(childPathStat[stat.ST_MODE])
