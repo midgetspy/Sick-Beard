8565696801b957baf2de13419710324c979065a3
Jonathon Saine <thezoggy@gmail.com>
2014-04-15 13:29:05 -0500

    Add some logic to notifiers js to catch empty required fields, prevent unneeded testing calls. Standardize and cleanup ajax calls as well.

--- data/js/configNotifications.js
+++ data/js/configNotifications.js
@@ -1,133 +1,187 @@
 $(document).ready(function () {
     var loading = '<img src="' + sbRoot + '/images/loading16.gif" height="16" width="16" />';
 
-    $('#testGrowl').click(function () {
-        $('#testGrowl-result').html(loading);
-        var growl_host = $("#growl_host").val();
-        var growl_password = $("#growl_password").val();
-        $.get(sbRoot + "/home/testGrowl", {'host': growl_host, 'password': growl_password},
-            function (data) { $('#testGrowl-result').html(data); });
+    $("#testGrowl").click(function () {
+        var growl_host = $.trim($("#growl_host").val());
+        var growl_password = $.trim($("#growl_password").val());
+        if (!growl_host) {
+            $("#testGrowl-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testGrowl-result").html(loading);
+        $.get(sbRoot + "/home/testGrowl", {'host': growl_host, 'password': growl_password})
+            .done(function (data) {
+                $("#testGrowl-result").html(data);
+                $("#testGrowl").attr("disabled", false);
+            });
     });
 
-    $('#testProwl').click(function () {
-        $('#testProwl-result').html(loading);
-        var prowl_api = $("#prowl_api").val();
+    $("#testProwl").click(function () {
+        var prowl_api = $.trim($("#prowl_api").val());
         var prowl_priority = $("#prowl_priority").val();
-        $.get(sbRoot + "/home/testProwl", {'prowl_api': prowl_api, 'prowl_priority': prowl_priority},
-            function (data) { $('#testProwl-result').html(data); });
+        if (!prowl_api) {
+            $("#testProwl-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testProwl-result").html(loading);
+        $.get(sbRoot + "/home/testProwl", {'prowl_api': prowl_api, 'prowl_priority': prowl_priority})
+            .done(function (data) {
+                $("#testProwl-result").html(data);
+                $("#testProwl").attr("disabled", false);
+            });
     });
 
-    $('#testXBMC').click(function () {
-        $("#testXBMC").attr("disabled", true);
-        $('#testXBMC-result').html(loading);
-        var xbmc_host = $("#xbmc_host").val();
-        var xbmc_username = $("#xbmc_username").val();
-        var xbmc_password = $("#xbmc_password").val();
+    $("#testXBMC").click(function () {
+        var xbmc_host = $.trim($("#xbmc_host").val());
+        var xbmc_username = $.trim($("#xbmc_username").val());
+        var xbmc_password = $.trim($("#xbmc_password").val());
+        if (!xbmc_host) {
+            $("#testXBMC-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testXBMC-result").html(loading);
         $.get(sbRoot + "/home/testXBMC", {'host': xbmc_host, 'username': xbmc_username, 'password': xbmc_password})
             .done(function (data) {
-                $('#testXBMC-result').html(data);
+                $("#testXBMC-result").html(data);
                 $("#testXBMC").attr("disabled", false);
             });
     });
 
-    $('#testPLEX').click(function () {
-        $('#testPLEX-result').html(loading);
-        var plex_host = $("#plex_host").val();
-        var plex_username = $("#plex_username").val();
-        var plex_password = $("#plex_password").val();
-        $.get(sbRoot + "/home/testPLEX", {'host': plex_host, 'username': plex_username, 'password': plex_password},
-            function (data) { $('#testPLEX-result').html(data); });
-    });
-
-    $('#testBoxcar').click(function () {
-        $('#testBoxcar-result').html(loading);
-        var boxcar_username = $("#boxcar_username").val();
-        $.get(sbRoot + "/home/testBoxcar", {'username': boxcar_username},
-            function (data) { $('#testBoxcar-result').html(data); });
-    });
-
-    $('#testPushover').click(function () {
-        $('#testPushover-result').html(loading);
-        var pushover_userkey = $("#pushover_userkey").val();
-        $.get(sbRoot + "/home/testPushover", {'userKey': pushover_userkey},
-            function (data) { $('#testPushover-result').html(data); });
-    });
-
-    $('#testLibnotify').click(function () {
-        $('#testLibnotify-result').html(loading);
-        $.get(sbRoot + "/home/testLibnotify",
-            function (data) { $('#testLibnotify-result').html(data); });
-    });
-
-    $('#twitterStep1').click(function () {
-        $('#testTwitter-result').html(loading);
-        $.get(sbRoot + "/home/twitterStep1", function (data) {window.open(data); })
-            .done(function () { $('#testTwitter-result').html('<b>Step1:</b> Confirm Authorization'); });
-    });
-
-    $('#twitterStep2').click(function () {
-        $('#testTwitter-result').html(loading);
-        var twitter_key = $("#twitter_key").val();
-        $.get(sbRoot + "/home/twitterStep2", {'key': twitter_key},
-            function (data) { $('#testTwitter-result').html(data); });
-    });
-
-    $('#testTwitter').click(function () {
-        $.get(sbRoot + "/home/testTwitter",
-            function (data) { $('#testTwitter-result').html(data); });
-    });
-
-    $('#settingsNMJ').click(function () {
-        if (!$('#nmj_host').val()) {
-            alert('Please fill in the Popcorn IP address');
-            $('#nmj_host').focus();
+    $("#testPLEX").click(function () {
+        var plex_host = $.trim($("#plex_host").val());
+        var plex_username = $.trim($("#plex_username").val());
+        var plex_password = $.trim($("#plex_password").val());
+        if (!plex_host) {
+            $("#testPLEX-result").html("Please fill out the necessary fields above.");
             return;
         }
-        $('#testNMJ-result').html(loading);
-        var nmj_host = $('#nmj_host').val();
+        $(this).attr("disabled", true);
+        $("#testPLEX-result").html(loading);
+        $.get(sbRoot + "/home/testPLEX", {'host': plex_host, 'username': plex_username, 'password': plex_password})
+            .done(function (data) {
+                $("#testPLEX-result").html(data);
+                $("#testPLEX").attr("disabled", false);
+            });
+    });
 
+    $("#testBoxcar").click(function () {
+        var boxcar_username = $.trim($("#boxcar_username").val());
+        if (!boxcar_username) {
+            $("#testBoxcar-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testBoxcar-result").html(loading);
+        $.get(sbRoot + "/home/testBoxcar", {'username': boxcar_username})
+            .done(function (data) {
+                $("#testBoxcar-result").html(data);
+                $("#testBoxcar").attr("disabled", false);
+            });
+    });
+
+    $("#testPushover").click(function () {
+        var pushover_userkey = $.trim($("#pushover_userkey").val());
+        if (!pushover_userkey) {
+            $("#testPushover-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testPushover-result").html(loading);
+        $.get(sbRoot + "/home/testPushover", {'userKey': pushover_userkey})
+            .done(function (data) {
+                $("#testPushover-result").html(data);
+                $("#testPushover").attr("disabled", false);
+            });
+    });
+
+    $("#testLibnotify").click(function () {
+        $("#testLibnotify-result").html(loading);
+        $.get(sbRoot + "/home/testLibnotify",
+            function (data) { $("#testLibnotify-result").html(data); });
+    });
+
+    $("#twitterStep1").click(function () {
+        $("#testTwitter-result").html(loading);
+        $.get(sbRoot + "/home/twitterStep1", function (data) {window.open(data); })
+            .done(function () { $("#testTwitter-result").html("<b>Step1:</b> Confirm Authorization"); });
+    });
+
+    $("#twitterStep2").click(function () {
+        var twitter_key = $.trim($("#twitter_key").val());
+        if (!twitter_key) {
+            $("#testTwitter-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $("#testTwitter-result").html(loading);
+        $.get(sbRoot + "/home/twitterStep2", {'key': twitter_key},
+            function (data) { $("#testTwitter-result").html(data); });
+    });
+
+    $("#testTwitter").click(function () {
+        $.get(sbRoot + "/home/testTwitter",
+            function (data) { $("#testTwitter-result").html(data); });
+    });
+
+    $("#settingsNMJ").click(function () {
+        var nmj_host = $.trim($("#nmj_host").val());
+        if (!nmj_host) {
+            alert("Please fill in the Popcorn IP address");
+            $("#nmj_host").focus();
+            return;
+        }
+        $("#testNMJ-result").html(loading);
         $.get(sbRoot + "/home/settingsNMJ", {'host': nmj_host},
             function (data) {
                 if (data === null) {
-                    $('#nmj_database').removeAttr('readonly');
-                    $('#nmj_mount').removeAttr('readonly');
+                    $("#nmj_database").removeAttr("readonly");
+                    $("#nmj_mount").removeAttr("readonly");
                 }
                 var JSONData = $.parseJSON(data);
-                $('#testNMJ-result').html(JSONData.message);
-                $('#nmj_database').val(JSONData.database);
-                $('#nmj_mount').val(JSONData.mount);
+                $("#testNMJ-result").html(JSONData.message);
+                $("#nmj_database").val(JSONData.database);
+                $("#nmj_mount").val(JSONData.mount);
 
                 if (JSONData.database) {
-                    $('#nmj_database').attr('readonly', true);
+                    $("#nmj_database").attr("readonly", true);
                 } else {
-                    $('#nmj_database').removeAttr('readonly');
+                    $("#nmj_database").removeAttr("readonly");
                 }
                 if (JSONData.mount) {
-                    $('#nmj_mount').attr('readonly', true);
+                    $("#nmj_mount").attr("readonly", true);
                 } else {
-                    $('#nmj_mount').removeAttr('readonly');
+                    $("#nmj_mount").removeAttr("readonly");
                 }
             });
     });
 
-    $('#testNMJ').click(function () {
-        $('#testNMJ-result').html(loading);
-        var nmj_host = $("#nmj_host").val();
+    $("#testNMJ").click(function () {
+        var nmj_host = $.trim($("#nmj_host").val());
         var nmj_database = $("#nmj_database").val();
         var nmj_mount = $("#nmj_mount").val();
-
-        $.get(sbRoot + "/home/testNMJ", {'host': nmj_host, 'database': nmj_database, 'mount': nmj_mount},
-            function (data) { $('#testNMJ-result').html(data); });
-    });
-
-    $('#settingsNMJv2').click(function () {
-        if (!$('#nmjv2_host').val()) {
-            alert('Please fill in the Popcorn IP address');
-            $('#nmjv2_host').focus();
+        if (!nmj_host) {
+            $("#testNMJ-result").html("Please fill out the necessary fields above.");
             return;
         }
-        $('#testNMJv2-result').html(loading);
-        var nmjv2_host = $('#nmjv2_host').val();
+        $(this).attr("disabled", true);
+        $("#testNMJ-result").html(loading);
+        $.get(sbRoot + "/home/testNMJ", {'host': nmj_host, 'database': nmj_database, 'mount': nmj_mount})
+            .done(function (data) {
+                $("#testNMJ-result").html(data);
+                $("#testNMJ").attr("disabled", false);
+            });
+    });
+
+    $("#settingsNMJv2").click(function () {
+        var nmjv2_host = $.trim($("#nmjv2_host").val());
+        if (!nmjv2_host) {
+            alert("Please fill in the Popcorn IP address");
+            $("#nmjv2_host").focus();
+            return;
+        }
+
         var nmjv2_dbloc;
         var radios = document.getElementsByName("nmjv2_dbloc");
         for (var i = 0; i < radios.length; i++) {
@@ -137,47 +191,70 @@ $(document).ready(function () {
             }
         }
 
-        var nmjv2_dbinstance=$('#NMJv2db_instance').val();
+        var nmjv2_dbinstance = $("#NMJv2db_instance").val();
+        $("#testNMJv2-result").html(loading);
         $.get(sbRoot + "/home/settingsNMJv2", {'host': nmjv2_host, 'dbloc': nmjv2_dbloc, 'instance': nmjv2_dbinstance},
-        function (data){
+        function (data) {
             if (data == null) {
-                $('#nmjv2_database').removeAttr('readonly');
+                $("#nmjv2_database").removeAttr("readonly");
             }
             var JSONData = $.parseJSON(data);
-            $('#testNMJv2-result').html(JSONData.message);
-            $('#nmjv2_database').val(JSONData.database);
-            
+            $("#testNMJv2-result").html(JSONData.message);
+            $("#nmjv2_database").val(JSONData.database);
+
             if (JSONData.database) {
-                $('#nmjv2_database').attr('readonly', true);
+                $("#nmjv2_database").attr("readonly", true);
             } else {
-                $('#nmjv2_database').removeAttr('readonly');
+                $("#nmjv2_database").removeAttr("readonly");
             }
         });
     });
 
-    $('#testNMJv2').click(function () {
-        $('#testNMJv2-result').html(loading);
-        var nmjv2_host = $("#nmjv2_host").val();
-        
-        $.get(sbRoot + "/home/testNMJv2", {'host': nmjv2_host},
-            function (data){ $('#testNMJv2-result').html(data); });
+    $("#testNMJv2").click(function () {
+        var nmjv2_host = $.trim($("#nmjv2_host").val());
+        if (!nmjv2_host) {
+            $("#testNMJv2-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testNMJv2-result").html(loading);
+        $.get(sbRoot + "/home/testNMJv2", {'host': nmjv2_host})
+            .done(function (data) {
+                $("#testNMJv2-result").html(data);
+                $("#testNMJv2").attr("disabled", false);
+            });
     });
 
-    $('#testTrakt').click(function () {
-        $('#testTrakt-result').html(loading);
-        var trakt_api = $("#trakt_api").val();
-        var trakt_username = $("#trakt_username").val();
-        var trakt_password = $("#trakt_password").val();
-
-        $.get(sbRoot + "/home/testTrakt", {'api': trakt_api, 'username': trakt_username, 'password': trakt_password},
-            function (data) { $('#testTrakt-result').html(data); });
+    $("#testTrakt").click(function () {
+        var trakt_api = $.trim($("#trakt_api").val());
+        var trakt_username = $.trim($("#trakt_username").val());
+        var trakt_password = $.trim($("#trakt_password").val());
+        if (!trakt_api || !trakt_username || !trakt_password) {
+            $("#testTrakt-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testTrakt-result").html(loading);
+        $.get(sbRoot + "/home/testTrakt", {'api': trakt_api, 'username': trakt_username, 'password': trakt_password})
+            .done(function (data) {
+                $("#testTrakt-result").html(data);
+                $("#testTrakt").attr("disabled", false);
+            });
     });
 
-    $('#testNMA').click(function () {
-        $('#testNMA-result').html(loading);
-        var nma_api = $("#nma_api").val();
+    $("#testNMA").click(function () {
+        var nma_api = $.trim($("#nma_api").val());
         var nma_priority = $("#nma_priority").val();
-        $.get(sbRoot + "/home/testNMA", {'nma_api': nma_api, 'nma_priority': nma_priority},
-            function (data) { $('#testNMA-result').html(data); });
+        if (!nma_api) {
+            $("#testNMA-result").html("Please fill out the necessary fields above.");
+            return;
+        }
+        $(this).attr("disabled", true);
+        $("#testNMA-result").html(loading);
+        $.get(sbRoot + "/home/testNMA", {'nma_api': nma_api, 'nma_priority': nma_priority})
+            .done(function (data) {
+                $("#testNMA-result").html(data);
+                $("#testNMA").attr("disabled", false);
+            });
     });
 });
