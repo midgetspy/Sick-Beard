3cf28750497aa7cddad3a23f94f207db76bd3afc
Patrick Vos <itofzo@gmail.com>
2014-05-13 23:48:53 +0200

    Change detection non seasons packs

--- sickbeard/name_parser/parser.py
+++ sickbeard/name_parser/parser.py
@@ -125,8 +125,8 @@ class NameParser(object):
                 if tmp_extra_info:
                     result.is_proper = re.search('(^|[\. _-])(proper|repack)([\. _-]|$)', tmp_extra_info, re.I) is not None
 
-                # Show.S04.Special is almost certainly not every episode in the season
-                if tmp_extra_info and cur_regex_name == 'season_only' and re.match(r'([. _-]|^)(special|extra)\w*([. _-]|$)', tmp_extra_info, re.I):
+                # Show.S04.Special or Show.S05.Part.2.Extras is almost certainly not every episode in the season
+                if tmp_extra_info and cur_regex_name == 'season_only' and re.search(r'([. _-]|^)(special|extra)s?\w*([. _-]|$)', tmp_extra_info, re.I):
                     continue
                 result.extra_info = tmp_extra_info
 
