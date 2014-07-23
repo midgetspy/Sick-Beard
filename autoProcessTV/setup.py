d965397fdc084a546aeff4246d64b1086a67f76d
Jonathon Saine <thezoggy@gmail.com>
2014-07-22 23:54:32 -0500

    Add icon and info to our sabToSickbeard binary, setting version to `0.0.0.1`.

--- autoProcessTV/setup.py
+++ autoProcessTV/setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
-import py2exe, sys, shutil
+import py2exe
+import sys
+import shutil
 
 sys.argv.append('py2exe')
 
 setup(
-      options = {'py2exe': {'bundle_files': 1}},
-#      windows = [{'console': "sabToSickbeard.py"}],
-      zipfile = None,
-      console = ['sabToSickbeard.py'],
+      options={'py2exe': {'bundle_files': 1}},
+      zipfile=None,
+      console=[{'script': 'sabToSickbeard.py',
+                'icon_resources': [(0, "../data/images/ico/sickbeard.ico")],
+                'version': '0.0.0.1',
+                'company_name': 'SickBeard-Team',
+                'name': 'sabToSickbeard',
+                'comments': 'sabToSickbeard - SABnzbd post-processing script',
+                'copyright': 'Copyright (C) 2009-2014 Nic Wolfe',
+                }],
 )
 
 shutil.copy('dist/sabToSickbeard.exe', '.')
