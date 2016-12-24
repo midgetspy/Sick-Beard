4f5247f52b677ebe22a40661019336875fccd49f
VeNoMouS <venom@gen-x.co.nz>
2016-12-24 23:42:16 +1300

    IPTorrents, fix _getPassKey with user id extraction.

--- sickbeard/providers/iptorrents.py
+++ sickbeard/providers/iptorrents.py
@@ -198,9 +198,7 @@ class IPTorrentsProvider(generic.TorrentProvider):
                 'cat[]': '5',
                 'feed': 'direct',
             }
-            rssData = re.findall(r'torrents\/rss\?u=(.*);tp=([0-9A-Fa-f]{32});', self.getURL(self.url + "getrss.php", post_params))[0]
-            self.rss_uid = rssData[0]
-            self.rss_passkey = rssData[1]
+            (self.rss_uid, self.rss_passkey)  = re.findall(r'torrents\/rss\?u=(\d+);tp=([0-9A-Fa-f]{32});', self.getURL(self.url + "getrss.php", post_params))[0]
         except:
             logger.log("[" + self.name + "] " + self.funcName() + " Failed to scrape authentication parameters for rss.", logger.ERROR)
             return False
